1fd7f7e7125ea5a96fc282486a480b0c8ea3937f
Daniel Szabo <dgszabo@gmail.com>
2018-02-21 19:29:30 -0800

    Daniel Testing - replaceWith completed

--- intermediate-js/02-testing/functionsSpec.js
+++ intermediate-js/02-testing/functionsSpec.js
@@ -1,2 +1,7 @@
 // Write your tests here!
-// the tests will be here
+describe("replaceWith", function() {
+    it("expect replaceWith to replace some character with another character", function() {
+        expect(replaceWith("awesome", "e", "z")).toBe("awzsomz");
+        expect(replaceWith("Foo", "F", "B")).toBe("Boo");
+    });
+});
