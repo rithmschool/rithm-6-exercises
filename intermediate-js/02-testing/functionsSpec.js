cfd697747298a1221535774a7c0aa719a25217da
Miranda Howitt <miranda.howitt@gmail.com>
2018-02-22 00:11:41 -0800

    2 tests

--- intermediate-js/02-testing/functionsSpec.js
+++ intermediate-js/02-testing/functionsSpec.js
@@ -3,5 +3,12 @@ describe("replaceIt function", function(){
     it("should return a string with replaced letter", function(){
         expect(replaceWith("awesome", "e", "z")).toBe("awzsomz");
         expect(replaceWith("Foo", "F", "B")).toBe("Boo");
-    })
-})
+    });
+});
+
+describe("expand function", function(){
+    it("returns a copy of the array with as many numbers as provided", function(){
+        expect(expand([1, 2, 3], 3)).toEqual([1,2,3,1,2,3,1,2,3]);
+        expect(expand(["foo", "test"], 1)).toEqual(["foo","test"]);
+    });
+});
