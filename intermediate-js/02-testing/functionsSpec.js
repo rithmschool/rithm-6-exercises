106f4ed8937d67e206cbd8622a0f9a6a43c615e2
Miranda Howitt <miranda.howitt@gmail.com>
2018-02-22 00:03:04 -0800

    testing start

--- intermediate-js/02-testing/functionsSpec.js
+++ intermediate-js/02-testing/functionsSpec.js
@@ -1,1 +1,7 @@
 // Write your tests here!
+describe("replaceIt function", function(){
+    it("should return a string with replaced letter", function(){
+        expect(replaceWith("awesome", "e", "z")).toBe("awzsomz");
+        expect(replaceWith("Foo", "F", "B")).toBe("Boo");
+    })
+})
