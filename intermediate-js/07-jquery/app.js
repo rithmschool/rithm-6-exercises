de6c83c94896f4769ba0cec714f427a627b48612
Niki Esfandiari <nesfandiari37@gmail.com>
2018-02-23 15:09:36 -0800

    almost complete 2

--- intermediate-js/07-jquery/app.js
+++ intermediate-js/07-jquery/app.js
@@ -11,37 +11,39 @@ $('.all').on('click', function(e){
 $('.favs').on('click', function(e){
     $('.all').toggleClass('hidden')
     $('.favs').toggleClass('hidden')
-    
     $('.news').find($('*')).filter($('.full.hidden')).closest('li').toggleClass('hidden')
 })
 
 // append new Links
 var count = 1
 $('.form').on('submit', function(e){
+    e.preventDefault();
     var title = $('#title').val();
     var url = $('#url').val();
-    e.preventDefault();
-
-    $('.news').append(
+    var lineItem = 
     `<li class='line'>
         <label>${count +1}.</label>
-        <div class='icons info'>
+        <div class='icons info' id='line${count+1}'>
             <i class="far fa-star empty"></i>
             <i class="fa fa-star hidden full"></i>
         </div>
         <a class='info' href="${url}">${title}</a>
-	    <a class='info' href="${url}">(${url})</a>
+        <a class='info' href="${url}">(${url.slice(8, -1)})</a>
     </li>`
-    )
+    $('.news').append(lineItem)
 
+    $(`#${count + 1}`).on('click', function(e){
+        $(this).children().each(function(){
+            $(this).toggleClass('hidden')
+        })
+    })
+    $('.form').toggleClass('hidden')
+    $('#title').val('');
+    $('#url').val('');
     count ++;
 })
 
-$(".fa-star").on("click", function() {
-    $(this).toggleClass("far fa-star fas fa-star");
-}); 
-
-$('.icons').on('click', function(e){
+$(`.icons`).on('click', function(e){
     $(this).children().each(function(){
         $(this).toggleClass('hidden')
     })
