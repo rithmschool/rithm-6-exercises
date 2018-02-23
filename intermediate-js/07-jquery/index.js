f3f6cd3b41318b649023f028984db0cebc14d63e
Alex Bush <alexabush7@gmail.com>
2018-02-22 17:19:26 -0800

    added number to each list item, form visibility toggles

--- intermediate-js/07-jquery/index.js
+++ intermediate-js/07-jquery/index.js
@@ -1,17 +1,38 @@
 var $ul = $('#list ul');
 var $form = $('form');
+var $toggleForm = $('#toggleForm')
 
+$toggleForm.on('click', function(e) {
+  if ($form.is(":visible")) $form.hide();
+  else $form.show();
+
+})
+
+
+sampleItems();
 
 var counter = 1;
 $form.on('submit', function(e) {
   e.preventDefault();
-  var $newLi = $('<li>');
-  var $number = $('<span>').text(counter)
-  var $titleTxt = $('<span>').text($('#title').val())
-  var $urlTxt = $('<span>').text($('#url').val())
-  var $newLi = $('<li>');
-  $newLi.append($number, $titleTxt, $urlTxt);
-  $ul.append($newLi);
+  var titleTxt = $('#title').val();
+  var urlTxt = $('#url').val();
+  addListItem(counter, titleTxt, urlTxt);
   counter++;
   $form.trigger("reset");
 })
+
+function addListItem(num, title, url) {
+  var $num = $('<span>').text(num)
+  var $titleTxt = $('<span>').text(title)
+  var $urlTxt = $('<span>').text(url)
+
+  var $newLi = $('<li>');
+  $newLi.append($num, $titleTxt, $urlTxt);
+  $ul.append($newLi);
+}
+
+function sampleItems() {
+  addListItem('1', 'a', 'blurb');
+  addListItem('2', 'b', 'blur');
+  addListItem('3', 'c', 'marco polo');
+}
