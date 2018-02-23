0684dc3870d26b767fe4f309a8b8f7c83827ccf3
Karl <karlsecco@gmail.com>
2018-02-22 17:03:11 -0800

    1/5 completed

--- intermediate-js/02-testing/functions.js
+++ intermediate-js/02-testing/functions.js
@@ -1,1 +1,21 @@
-// Write your functions here!
+// Ex 1
+
+function replaceWith(str, target, val) {
+  var newStr = '';
+  for (let i = 0; i < str.length; i++) {
+    newStr += str[i] === target ? val : str[i];
+  }
+  return newStr;
+}
+
+replaceWith('awesome', 'e', 'z'); // "awzsomz"
+replaceWith('Foo', 'F', 'B'); // "Boo"
+replaceWith('foo', 'F', 'B'); // "foo"
+replaceWith('awesome', 'z', 'e'); // "awesome"
+
+// Ex 2
+
+function expand(arr, n) {}
+
+expand([1, 2, 3], 3); //[1,2,3,1,2,3,1,2,3]
+expand(['foo', 'test'], 1); //["foo","test"]
--- intermediate-js/02-testing/functionsSpec.js
+++ intermediate-js/02-testing/functionsSpec.js
@@ -1,60 +1,70 @@
-describe("replaceWith", function() {
-    it("it should replace the target character in a string with another input character", function() {
-        expect(replaceWith("awesome", "e", "z")).toBe("awzsomz");
-        expect(replaceWith("Foo", "F", "B")).toBe("Boo");
-    });
+describe('replaceWith', function() {
+  it('it should replace the target input with the input value', function() {
+    expect(replaceWith('awesome', 'e', 'z')).toBe('awzsomz');
+    expect(replaceWith('Foo', 'F', 'B')).toBe('Boo');
+  });
+  it('it should be case sensitive', function() {
+    expect(replaceWith('Foo', 'F', 'B')).toBe('Boo');
+    expect(replaceWith('foo', 'F', 'B')).toBe('foo');
+  });
+  it('it should return a string', function() {
+    expect(typeof replaceWith('awesome', 'e', 'z')).toBe('string');
+  });
+  it('it should return a copy of the input string if the input string does not include the target input', function() {
+    expect(replaceWith('awesome', 'z', 'e')).toBe('awesome');
+  });
 });
 
-describe("expand", function() {
-    it("it should return a copy of the input array with its elements repeated n times", function() {
-        expect(expand([1, 2, 3], 3)).toEqual([1,2,3,1,2,3,1,2,3]);
-        expect(expand(["foo", "test"], 1)).toEqual(["foo","test"]);
-    });
+describe('expand', function() {
+  it('it should return a copy of the input array with its elements repeated n times', function() {
+    expect(expand([1, 2, 3], 3)).toEqual([1, 2, 3, 1, 2, 3, 1, 2, 3]);
+    expect(expand(['foo', 'test'], 1)).toEqual(['foo', 'test']);
+  });
 });
 
-describe("acceptNumbersOnly", function() {
-    it("it should return true if all inputs are numbers", function() {
-        expect(acceptNumbersOnly(1, "foo")).toEqual(false);
-        expect(acceptNumbersOnly(1, 2, 3, 4, 5, 6, 7)).toEqual(true);
-        expect(acceptNumbersOnly(1, 2, 3, 4, 5, 6, NaN)).toEqual(false);
-    });
-});
+// describe("acceptNumbersOnly", function() {
+//     it("it should return true if all inputs are numbers", function() {
+//         expect(acceptNumbersOnly(1, "foo")).toEqual(false);
+//         expect(acceptNumbersOnly(1, 2, 3, 4, 5, 6, 7)).toEqual(true);
+//         expect(acceptNumbersOnly(1, 2, 3, 4, 5, 6, NaN)).toEqual(false);
+//     });
+// });
 
-describe("mergeArrays", function() {
-    it("it should return a single sorted array consisting of all elements in the input arrays", function() {
-        expect(mergeArrays([2, 1], [3, 4])).toEqual([1,2,3,4]);
-        expect(mergeArrays([2, 1], ['three', 'four', 'FIVE'])).toEqual([1,2,'three','four','FIVE']);
-    });
-});
+// describe("mergeArrays", function() {
+//     it("it should return a single sorted array consisting of all elements in the input arrays", function() {
+//         expect(mergeArrays([2, 1], [3, 4])).toEqual([1,2,3,4]);
+//         expect(mergeArrays([2, 1], ['three', 'four', 'FIVE'])).toEqual([1,2,'three','four','FIVE']);
+//     });
+// });
 
-describe("mergeObjects", function() {
-    var obj1 = {
-        name: "Foo",
-        num: 33
-    };
-    var obj2 = {
-        test: "thing",
-        luckyNum: 18
-    };
-    var obj3 = {
-        test: "thing",
-        num: 55
-    };
-    var obj4 = {
-        name: "Foo",
-        test: "thing",
-        num: 55,
-        luckyNum: 18
-    };
-    var obj5 = {
-        name = "Foo",
-        test: "thing",
-        num: 55
-    };
-    it("it should return an object combining the keys and values of both objects", function() {
-        expect(mergeObjects(obj1, obj2)).toEqual(obj3);
-    });
-    it("it should adopt the value of the second object for same key conflicts", function() {
-        expect(mergeObjects(obj1, obj2)).toEqual(obj3);
-    });
-});
+// describe("mergeObjects", function() {
+//     var obj1 = {
+//         name: "Foo",
+//         num: 33
+//     };
+//     var obj2 = {
+//         test: "thing",
+//         luckyNum: 18
+//     };
+//     var obj3 = {
+//         test: "thing",
+//         num: 55
+//     };
+//     var obj4 = {
+//         name: "Foo",
+//         test: "thing",
+//         num: 55,
+//         luckyNum: 18
+//     };
+//     var obj5 = {
+//         name = "Foo",
+//         test: "thing",
+//         num: 55
+//     };
+//     it("it should return an object combining the keys and values of both objects", function() {
+//         expect(mergeObjects(obj1, obj2)).toEqual(obj4);
+//     });
+//     it("it should adopt the value of the second object for same key conflicts", function() {
+//         expect(mergeObjects(obj1, obj3)).toEqual(obj5);
+//     });
+// });
