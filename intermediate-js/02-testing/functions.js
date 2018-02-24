7cbf2637a2a0509a2ee097e0aa798c4429f9aaf7
maxnawa31 <mnawa31@gmail.com>
2018-02-23 19:59:39 -0800

    finished up to contains

--- intermediate-js/02-testing/functions.js
+++ intermediate-js/02-testing/functions.js
@@ -1,1 +1,10 @@
 // Write your functions here!
+function replaceWith(string, original, replacement){
+    var array = string.split("");
+    for(var i =0; i<array.length; i++){
+        if(array[i].toLowerCase() === original.toLowerCase()){
+            array[i] = replacement;
+        }
+    }
+    return array.join("");
+}
--- intermediate-js/02-testing/functionsSpec.js
+++ intermediate-js/02-testing/functionsSpec.js
@@ -1,1 +1,13 @@
 // Write your tests here!
+
+describe("string",function(){
+    it("is lowercase", function(){
+        expect(replaceWith("awesome","e", "z")).toEqual("awzsomz");
+        expect(replaceWith("Foo", "F", "B")).toEqual("Boo");
+    })
+    it("is a string", function(){
+        expect(typeof "awesome","e","z").toEqual("string");
+        expect(typeof "Foo","F","B").toEqual("string");
+    });
+
+})
