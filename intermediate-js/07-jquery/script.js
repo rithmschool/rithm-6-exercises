8bd100118b41420d7224a2cbb3d8d9cf928ee3da
Miranda Howitt <miranda.howitt@gmail.com>
2018-02-23 16:27:20 -0800

    almost done

--- intermediate-js/07-jquery/script.js
+++ intermediate-js/07-jquery/script.js
@@ -1,6 +1,20 @@
 $(function() {
+  $("form").hide();
   $("#submit-nav").on("click", function() {
-    $($form).toggleClass("show-form");
+    $($form).slideDown(1000);
+  });
+
+  $("#fav-nav").on("click", function() {
+    //debugger;
+    event.preventDefault();
+    var $favNavTitle = $(this);
+    if ($favNavTitle.text() == $favNavTitle.data("text-swap")) {
+      $favNavTitle.text($favNavTitle.data("text-original"));
+    } else {
+      $favNavTitle.data("text-original", $favNavTitle.text());
+      $favNavTitle.text($favNavTitle.data("text-swap"));
+    }
+    $("li:not(.favorite)").toggleClass("dont-display");
   });
 
   var $form = $("form");
@@ -8,19 +22,26 @@ $(function() {
     event.preventDefault();
     var $titleVal = $("#title").val();
     var $url = $("#url").val();
+    var $urlSplit = $url.split("/");
+    var $hostUrl = $urlSplit[2];
 
     var $newArticle = $("<li>", {
       html: `
       <span><i class="far fa-star fa-sm" style="color:lightgrey"></i>
       </span>
-      ${$titleVal} <span><a>
+      ${$titleVal} <span><a href="${$url}" target="_blank" class="text-muted">&nbsp;(${$hostUrl})</a>
     `
     });
     $("ol").append($newArticle);
     $form.trigger("reset");
+    $form.slideUp(1000);
   });
 
   $("ol").on("click", ".fa-star", function(event) {
     $(event.target).toggleClass("far fa-star fas fa-star");
+
+    $(event.target)
+      .closest("li")
+      .toggleClass("favorite");
   });
 });
