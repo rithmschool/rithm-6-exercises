6083a9df10df2467ee8041073e620e8762ac267c
Alex Bush <alexabush7@gmail.com>
2018-02-23 17:11:54 -0800

    added bootstrap styling

--- intermediate-js/07-jquery/index.js
+++ intermediate-js/07-jquery/index.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-  var $ul = $('#list ul');
+  var $ol = $('#list ol');
   var $form = $('form');
   var $toggleForm = $('#toggleForm')
   var $toggleFavorites = $('#toggleFavorites')
-  let counter = 1;
 
   sampleItems();
 
@@ -19,34 +18,33 @@ $(document).ready(function() {
     e.preventDefault();
     var titleTxt = $('#title').val();
     var urlTxt = $('#url').val();
-    addListItem(counter, titleTxt, urlTxt);
-    counter++;
+    addListItem(titleTxt, urlTxt);
     $form.trigger("reset");
   })
 
 
-  $ul.on('click', '#starContainer', function(e) {
+  $ol.on('click', '#starContainer', function(e) {
     $(this).children().each(function() {
       $(this).toggleClass('hidden');
     });
   })
 
-  function addListItem(num, title, url) {
+  function addListItem(title, url) {
     var $solidStarDiv = ($('<span>').addClass('hidden')).append($('<i class=\"fas fa-star\"><i>'));
     var $hollowStarDiv = $('<span>').append($('<i class=\"far fa-star\"><i>'));
     var $starContainer = $('<span>').attr('id', 'starContainer').append($solidStarDiv, $hollowStarDiv);
-    var $num = $('<span>').text(num)
+    // var $num = $('<span>').text(num)
     var $titleTxt = $('<span>').text(title)
     var $urlTxt = $('<span>').text(url)
     var $newLi = $('<li>');
-    $newLi.append($num, $starContainer, $titleTxt, $urlTxt);
-    $ul.append($newLi);
+    $newLi.append($starContainer, $titleTxt, $urlTxt);
+    $ol.append($newLi);
   }
 
   function sampleItems() {
-    addListItem('1', 'a', 'blurb');
-    addListItem('2', 'b', 'blur');
-    addListItem('3', 'c', 'marco polo');
+    addListItem('a', 'blurb');
+    addListItem('b', 'blur');
+    addListItem('c', 'marco polo');
   }
 
 });
