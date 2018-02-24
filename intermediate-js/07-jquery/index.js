d55055ee3f8e6826e5fe10a8eb30e17e511cba8d
Alex Bush <alexabush7@gmail.com>
2018-02-23 19:06:02 -0800

    refactored for better naming conventions

--- intermediate-js/07-jquery/index.js
+++ intermediate-js/07-jquery/index.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
   var $ol = $('#list ol');
   var $form = $('form');
-  var $toggleForm = $('#toggleForm')
-  var $toggleFavorites = $('#toggleFavorites')
 
   sampleItems();
 
-  $toggleForm.on('click', function(e) {
+  $('#toggleForm').on('click', function(e) {
     $form.toggleClass('hidden');
   })
 
-  $toggleFavorites.on('click', function(e) {
-    console.log('works');
+  $('#toggleFavorites').on('click', function(e) {
+    var $data = $ol.find('svg').filter('.fa-star')
+    $data.closest('span').toggleClass('hidden');
+
   })
 
   $form.on('submit', function(e) {
@@ -23,17 +23,17 @@ $(document).ready(function() {
   })
 
 
-  $ol.on('click', '#starContainer', function(e) {
+  $ol.on('click', '#star-container', function(e) {
     $(this).children().each(function() {
       $(this).toggleClass('hidden');
     });
   })
 
   function addListItem(title, url) {
-    var $solidStarDiv = ($('<span>').addClass('hidden')).append($('<i class=\"fas fa-star\"><i>'));
+    var $solidStarDiv = ($('<span>').addClass('hidden solid-star')).append($('<i class=\"fas fa-star\"><i>'));
     var $hollowStarDiv = $('<span>').append($('<i class=\"far fa-star\"><i>'));
-    var $starContainer = $('<span>').attr('id', 'starContainer').append($solidStarDiv, $hollowStarDiv);
-    var $titleTxt = $('<span>').addClass('titleTxt').text(title)
+    var $starContainer = $('<span>').attr('id', 'star-container').append($solidStarDiv, $hollowStarDiv);
+    var $titleTxt = $('<span>').addClass('title-txt').text(title)
     var $urlTxt = $('<span>').addClass('urlTxt').text(`(${url})`)
     var $newLi = $('<li>');
     $newLi.append($starContainer, $titleTxt, $urlTxt);
@@ -41,9 +41,9 @@ $(document).ready(function() {
   }
 
   function sampleItems() {
-    addListItem('a', 'blurb');
-    addListItem('b', 'blur');
-    addListItem('c', 'marco polo');
+    addListItem('Visualizing Algorithms', 'visualgo.net');
+    addListItem('Problem Solving with Rithm', 'rithmschool.com');
+    addListItem('Buy this Book!', 'amazon.com');
   }
 
 });
