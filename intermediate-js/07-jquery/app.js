e138944a9546db64ce7f90fef55922303847ec7b
maxnawa31 <mnawa31@gmail.com>
2018-02-23 17:51:39 -0800

    parsed url

--- intermediate-js/07-jquery/app.js
+++ intermediate-js/07-jquery/app.js
@@ -4,9 +4,13 @@ var $link = $("#link");
 var $form = $("form");
 var $favorites = $("#favorites");
 var $submit = $("#submit");
-
+var $all = $("#all");
+function getRootUrl(url) {
+    return url.toString().replace(/^(.*\/\/[^\/?#]*).*$/,"$1");
+  }
 $(document).ready(function(){
     console.log("hello")
+    $form.hide();   
 })
 $form.on("submit", function(event){
     event.preventDefault();
@@ -17,7 +21,8 @@ $form.on("submit", function(event){
     $post.append($star);
     $titleText.addClass("larger-text").text($title.val());
     $post.append($titleText);
-    $url.text("(" + $link.val() + ")");
+    var parsedLink = getRootUrl($link.val())
+    $url.text("(" + parsedLink + ")");
     $list.append($post.append($url));
     $form.toggle("hide");
 })
@@ -34,6 +39,12 @@ $submit.on("click", function(){
 });
 
 $favorites.on("click", function(){
-    $form.toggle("hide")
-    $(".far").parent().hide();
+    if($all.text() === "Favorites"){
+        $all.text("All");
+        $(".far").parent().hide();
+    }else{
+        $all.text("Favorites");
+        $(".far").parent().show();
+    }
+    
 });
