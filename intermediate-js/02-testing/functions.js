ce58596e898d2a0f1dc85448ef52df1153fe0dea
Charmaine Estacio <charmaine.estacio@gmail.com>
2018-02-23 17:48:08 -0800

    adding tests to first function

--- intermediate-js/02-testing/functions.js
+++ intermediate-js/02-testing/functions.js
@@ -1,1 +1,9 @@
-// Write your functions here!
+function replaceWith(str, char1, char2) {
+    var arr = str.split("");
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === char1) {
+            arr[i] = char2;
+        }
+    }
+    return arr.join("");
+}
--- intermediate-js/02-testing/functionsSpec.js
+++ intermediate-js/02-testing/functionsSpec.js
@@ -1,1 +1,14 @@
-// Write your tests here!
+// failing tests
+describe("replaceWith", function() {
+    it("expects inputs to be a string and two characters", function() {
+        expect(replaceWith(123, 5, 6)).toBe(undefined);
+    });
+});
+
+//passing tests
+describe("replaceWith", function() {
+    it("expects the third input to replace the second input in the first string", function() {
+        expect(replaceWith("awesome", "e", "z")).toBe("awzsomz");
+        expect(replaceWith("Foo", "F", "B")).toBe("Boo");
+    });
+});
