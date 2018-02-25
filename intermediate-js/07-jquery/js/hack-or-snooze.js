90be0d3a5a86be55caf7e4c9cfc867f6b843607c
Daniel Szabo <dgszabo@gmail.com>
2018-02-25 00:25:14 -0800

    Daniel jQuery - regular / favorites switching func added

--- intermediate-js/07-jquery/js/hack-or-snooze.js
+++ intermediate-js/07-jquery/js/hack-or-snooze.js
@@ -1,5 +1,42 @@
 $(document).ready(function() {
+    var isSubmissionFormOpen = true;
     $('#submit-btn').on('click', function() {
         $('form').slideToggle();
+        isSubmissionFormOpen = !isSubmissionFormOpen;
+    });
+    $('#fav-all-btn').on('click', function() {
+        if(isSubmissionFormOpen) {
+            if($('ol').css('display') === 'block') {
+                $('form').fadeToggle();
+                $('ol').fadeToggle(function() {
+                    $('ul').fadeToggle();
+                });
+            } else {
+                $('ul').fadeToggle(function() {
+                    $('ol').fadeToggle();
+                    $('form').fadeToggle();
+                });
+            }
+        } else {
+            if($('ol').css('display') === 'block') {
+                $('ol').fadeToggle(function() {
+                    $('ul').fadeToggle();
+                });
+            } else {
+                $('ul').fadeToggle(function() {
+                    $('ol').fadeToggle();
+                });
+            }
+        }
+        
+        
+        
+    });
+    $('li').on('click', 'svg', function() {
+        if($(this).attr('data-prefix') === 'fas') {
+            $(this).attr('data-prefix', 'far');
+        } else {
+            $(this).attr('data-prefix', 'fas');
+        }
     });
 });
