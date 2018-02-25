f1eb51f1f8bf0037320d42c8f1a3aef8260991df
Karl <karlsecco@gmail.com>
2018-02-25 15:20:21 -0800

    completing tests cases and functions

--- intermediate-js/02-testing/functions.js
+++ intermediate-js/02-testing/functions.js
@@ -8,14 +8,37 @@ function replaceWith(str, target, val) {
   return newStr;
 }
 
-replaceWith('awesome', 'e', 'z'); // "awzsomz"
-replaceWith('Foo', 'F', 'B'); // "Boo"
-replaceWith('foo', 'F', 'B'); // "foo"
-replaceWith('awesome', 'z', 'e'); // "awesome"
-
 // Ex 2
 
-function expand(arr, n) {}
+function expand(arr, n) {
+  var copy = [];
+  for (var i = 0; i < n; i++) {
+    copy = copy.concat(arr);
+  }
+  return copy;
+}
 
-expand([1, 2, 3], 3); //[1,2,3,1,2,3,1,2,3]
-expand(['foo', 'test'], 1); //["foo","test"]
+// Ex 3
+
+function acceptNumbersOnly(...args) {
+  for (var i = 0; i < args.length; i++) {
+    if (typeof args[i] !== 'number' || isNaN(args[i])) return false;
+  }
+  return true;
+}
+
+// Ex 4
+
+function mergeArrays(arr1, arr2) {
+  return arr1.concat(arr2).sort((a, b) => a - b);
+}
+
+// Ex 5
+
+function mergeObjects(obj1, obj2) {
+  // return Object.assign(obj1, obj2);
+  var newObj = {};
+  for (let key1 in obj1) newObj[key1] = obj1[key1];
+  for (let key2 in obj2) newObj[key2] = obj2[key2];
+  return newObj;
+}
