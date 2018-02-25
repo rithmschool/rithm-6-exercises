766d08bf1f6f01a8fec8e9a06498cf120ffe38ed
maxnawa31 <mnawa31@gmail.com>
2018-02-25 11:22:38 -0800

    testing in progress

--- intermediate-js/02-testing/functions.js
+++ intermediate-js/02-testing/functions.js
@@ -1,1 +1,28 @@
 // Write your functions here!
+function replaceWith(string,original,replacement){
+    var arr = string.split("");
+    for(var i =0; i<arr.length; i++){
+        if(arr[i] === original){
+            arr[i] = replacement;
+        }
+    }
+    return arr.join("");
+}
+
+function expand(array,number){
+    for(var i =0; i<number;i++){
+        array = array.concat(array);
+    }
+    return array;
+}
+
+function acceptNumbersOnly(){
+    for(var i =0; i<arguments.length; i++){
+        if(arguments[i] === NaN){
+            return false;
+        }else if(typeof arguments[i] !== "number"){
+            return false;
+        }
+    }
+    return true;
+}
--- intermediate-js/02-testing/functionsSpec.js
+++ intermediate-js/02-testing/functionsSpec.js
@@ -1,1 +1,39 @@
 // Write your tests here!
+describe("string",function(){
+    it("is lowercase", function(){
+        expect(replaceWith("awesome","e", "z")).toEqual("awzsomz");
+        expect(replaceWith("Foo", "F", "B")).toEqual("Boo");
+    })
+    it("is a string", function(){
+        expect(typeof "awesome","e","z").toEqual("string");
+        expect(typeof "Foo","F","B").toEqual("string");
+    });
+
+})
+
+//function expand
+describe("array", function(){
+    it("is an array", function(){
+        expect(Array.isArray([1, 2, 3]).toEqual(true));
+        expect(Array.isArray(["foo", "test"]).toEqual(true));
+        expect(expand([1, 2, 3],3)).toEqual([1,2,3,1,2,3,1,2,3]);
+        expect(expand(["foo", "test"],1)).toEqual(["foo","test"])
+    })
+})
+
+describe("number", function(){
+    it("is a number", function(){
+        expect(typeof 1).toEqual("number");
+        expect(typeof 3).toEqual("number");
+    })
+})
+
+//function acceptNumbersOnly
+
+describe("numbers only", function(){
+    it("is a number", function(){
+        expect(acceptNumbersOnly(1, "foo").toEqual(false));
+        expect(acceptNumbersOnly(1, 2, 3, 4, 5, 6, 7).toEqual(false));
+        expect(acceptNumbersOnly(1, 2, 3, 4, 5, 6, NaN)).toEqual(false);
+    })
+})
