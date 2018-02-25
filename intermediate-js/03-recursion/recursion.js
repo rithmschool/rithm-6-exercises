4ee263fcf563363a787bcaa5684e1d73433cc770
Alex Bush <alexabush7@gmail.com>
2018-02-25 11:58:07 -0800

    updated stringifyNumbers() to return a new object

--- intermediate-js/03-recursion/recursion.js
+++ intermediate-js/03-recursion/recursion.js
@@ -8,6 +8,7 @@ function productOfArray(arr) {
 
 function collectStrings(obj) {
   var newArr = [];
+
   function helper(obj) {
     for (var element in obj) {
       if (typeof obj[element] === 'string') newArr.push(obj[element]);
@@ -18,16 +19,31 @@ function collectStrings(obj) {
   return newArr;
 }
 
-function stringifyNumbers(obj) {
-  for (var element in obj) {
-    if (isObject(obj[element])) stringifyNumbers(obj[element])
-    if (typeof obj[element] === 'number') obj[element] = obj[element].toString();
-  }
-  return obj
+// function stringifyNumbers(obj) {
+//   for (var element in obj) {
+//     if (isObject(obj[element])) stringifyNumbers(obj[element])
+//     if (typeof obj[element] === 'number') obj[element] = obj[element].toString();
+//   }
+//   return obj
 
-  function isObject (item) {
-    return (typeof item === "object" && !Array.isArray(item) && item !== null);
+//   function isObject (item) {
+//     return (typeof item === "object" && !Array.isArray(item) && item !== null);
+//   }
+// }
+
+function stringifyNumbers(originalObj) {
+  const newObj = JSON.parse(JSON.stringify(originalObj));
+  function helper(obj) {
+    for (var element in obj) {
+      if (isObject(obj[element])) helper(obj[element])
+      if (typeof obj[element] === 'number') obj[element] = obj[element].toString();
+    }
+    return obj;
+    function isObject(item) {
+      return (typeof item === "object" && !Array.isArray(item) && item !== null);
+    }
   }
+  return helper(newObj);
 }
 
 function contains(obj, value, flag = false) {
@@ -39,10 +55,10 @@ function contains(obj, value, flag = false) {
       }
     }
   }
-  helper(obj,value);
+  helper(obj, value);
   return flag;
 
-  function isObject (item) {
+  function isObject(item) {
     return (typeof item === "object" && !Array.isArray(item) && item !== null);
   }
 }
@@ -50,6 +66,7 @@ function contains(obj, value, flag = false) {
 function search(arr, value) {
   var count = 0;
   var max = arr.length;
+
   function helper(arr, value) {
     if (arr[0] === value) return true;
     if (count >= max) return false;
@@ -63,24 +80,15 @@ function search(arr, value) {
 }
 
 function binarySearch(arr, target, low = 0, high = arr.length - 1) {
-   var mid = getMid(low, high)
-   if (arr[mid] === target) {
-     return mid;
-   } 
-   if (low >= high) {
-     return -1;
-   }
-   else if (arr[mid] < target) {
-     return binarySearch(arr, target, mid + 1, high);
-   }
-   else {
-     return binarySearch(arr, target, low, mid - 1);    
-   } 
+  var mid = Math.floor(low + (high - low) / 2);
+  if (arr[mid] === target) {
+    return mid;
+  }
+  if (low >= high) {
+    return -1;
+  } else if (arr[mid] < target) {
+    return binarySearch(arr, target, mid + 1, high);
+  } else {
+    return binarySearch(arr, target, low, mid - 1);
+  }
 }
-
-
-
-
-
-
-
