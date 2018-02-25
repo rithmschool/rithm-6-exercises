32f8d9109bac9d207672f53bc88d2c56f60b65f0
Niki Esfandiari <nesfandiari37@gmail.com>
2018-02-25 13:05:48 -0800

    favorite bug fix/url string fix

--- intermediate-js/07-jquery/app.js
+++ intermediate-js/07-jquery/app.js
@@ -1,50 +1,52 @@
-$('.submitNav').on('click', function(e){
-    $('.form').toggleClass('hidden')
-})
+$(document).ready(function(){
 
-$('.all').on('click', function(e){
-    $('.all').toggleClass('hidden')
-    $('.favs').toggleClass('hidden')
-    $('.news').find($('*')).filter($('.full.hidden')).closest('li').toggleClass('hidden')
-})
+    // opening submission form
+    $('.submitNav').on('click', function(e){
+        $('.form').toggleClass('hidden')
+    })
 
-$('.favs').on('click', function(e){
-    $('.all').toggleClass('hidden')
-    $('.favs').toggleClass('hidden')
-    $('.news').find($('*')).filter($('.full.hidden')).closest('li').toggleClass('hidden')
-})
+    // filter for favorites
+    $('.all').on('click', function(e){
+        $('.all').toggleClass('hidden')
+        $('.favs').toggleClass('hidden')
+        $('.news').find($('*')).filter($('.full.hidden')).closest('li').toggleClass('hidden')
+    })
 
-// append new Links
-var count = 1
-$('.form').on('submit', function(e){
-    e.preventDefault();
-    var title = $('#title').val();
-    var url = $('#url').val();
-    var lineItem = 
-    `<li class='line'>
-        <label>${count +1}.</label>
-        <div class='icons info' id='line${count+1}'>
-            <i class="far fa-star empty"></i>
-            <i class="fa fa-star hidden full"></i>
-        </div>
-        <a class='info' href="${url}">${title}</a>
-        <a class='info' href="${url}">(${url.slice(8, -1)})</a>
-    </li>`
-    $('.news').append(lineItem)
+    // filter back to everything
+    $('.favs').on('click', function(e){
+        $('.all').toggleClass('hidden')
+        $('.favs').toggleClass('hidden')
+        $('.news').find($('*')).filter($('.full.hidden')).closest('li').toggleClass('hidden')
+    })
 
-    $(`#${count + 1}`).on('click', function(e){
+    // append new Lines from form submission
+    var count = 1
+    $('.form').on('submit', function(e){
+        e.preventDefault();
+        var title = $('#title').val();
+        var url = $('#url').val();
+        var lineItem = 
+        `<li class='line'>
+            <label>${count +1}.</label>
+            <div class='icons info' id='line${count+1}'>
+                <i class="far fa-star empty"></i>
+                <i class="fa fa-star hidden full"></i>
+            </div>
+            <a class='info' href="${url}">${title}</a>
+            <a class='info' href="${url}">(${url.slice(url.indexOf("www") + 4, url.indexOf(".com") + 4)})</a>
+        </li>`
+
+        $('.news').append(lineItem)
+        $('.form').toggleClass('hidden')
+        $('#title').val('');
+        $('#url').val('');
+        count ++;
+    })
+
+    // favorite a line item
+    $(`.news`).on('click', '.icons', function(e){
         $(this).children().each(function(){
             $(this).toggleClass('hidden')
         })
     })
-    $('.form').toggleClass('hidden')
-    $('#title').val('');
-    $('#url').val('');
-    count ++;
-})
-
-$(`.icons`).on('click', function(e){
-    $(this).children().each(function(){
-        $(this).toggleClass('hidden')
-    })
-})
+});
