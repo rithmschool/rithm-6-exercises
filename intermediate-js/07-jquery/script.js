75b599e25748880a4c77675ab09a128673616eee
Kelson Warner <kelsonpw35@gmail.com>
2018-02-25 20:24:23 -0800

    all functionality complete except for bonus solution that allows clicking on the host name to show all listings from that host, framework is essentially in place for that, will continue to work on it.  everything else is complete :)

--- intermediate-js/07-jquery/script.js
+++ intermediate-js/07-jquery/script.js
@@ -1,22 +1,63 @@
 var $form = $("form");
 
+function isUrl(s) {
+  var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+  return regexp.test(s);
+}
+function extractHostname(url) {
+  var hostname;
+  if (url.indexOf("://") > -1) {
+    hostname = url.split("/")[2];
+  } else {
+    hostname = url.split("/")[0];
+  }
+
+  hostname = hostname.split(":")[0];
+  hostname = hostname.split("?")[0];
+
+  return hostname;
+}
+
+function extractRootDomain(url) {
+  var domain = extractHostname(url);
+  var splitArr = domain.split(".");
+  var arrLen = splitArr.length;
+
+  if (arrLen > 2) {
+    domain = splitArr[arrLen - 2] + "." + splitArr[arrLen - 1];
+    if (splitArr[arrLen - 1].length == 2 && splitArr[arrLen - 1].length == 2) {
+      domain = splitArr[arrLen - 3] + "." + domain;
+    }
+  }
+  return domain;
+}
+
 $form.on("submit", function(event) {
   event.preventDefault();
   var $star = $('<i class="far fa-star"></i>');
   $star.attr("aria-hidden", "true");
   var $title = $("#title").val();
   var $url = $("#url").val();
-  var $newLi = $("<li>", {
-    class: "listing favfalse"
-  });
-  $newLi.append($star);
-  var $newUrl = $("<small>", {
-    text: "(" + $url + ")",
-    class: "listing__url ml-2"
-  });
-  $($newLi).append($newUrl);
-  $("ol").append($newLi);
-  $form.trigger("reset");
+  if (!isUrl($url)) {
+    alert("Invalid URL Formatting.");
+  } else {
+    var $newLi = $("<li>", {
+      class: "listing favfalse"
+    });
+    $newLi.append($star);
+    $newLi.append("<a href=" + $url + " class='titlelink'>" + $title + "</a>");
+    var domain = extractRootDomain($url);
+    var $newUrl = $("<a>", {
+      text: "(" + domain + ")",
+      href: domain,
+      class: "listing__url ml-2"
+    });
+
+    $($newLi).append($newUrl);
+    $("ol").append($newLi);
+
+    $form.trigger("reset");
+  }
 });
 
 $("#submitbutton").click(function(event) {
