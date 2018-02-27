1b92c76f0b2d8815352872adb6222b8f9aa9a9fe
Kelson Warner <kelsonpw35@gmail.com>
2018-02-26 16:51:41 -0800

    fixed a few issues

--- intermediate-js/07-jquery/script.js
+++ intermediate-js/07-jquery/script.js
@@ -1,76 +1,92 @@
-var $form = $("form");
-
-function isUrl(s) {
-  var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
-  return regexp.test(s);
-}
-function extractHostname(url) {
-  var hostname;
-  if (url.indexOf("://") > -1) {
-    hostname = url.split("/")[2];
-  } else {
-    hostname = url.split("/")[0];
+$(document).ready(function() {
+  function isUrl(s) {
+    var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+    return regexp.test(s);
   }
-
-  hostname = hostname.split(":")[0];
-  hostname = hostname.split("?")[0];
-
-  return hostname;
-}
-
-function extractRootDomain(url) {
-  var domain = extractHostname(url);
-  var splitArr = domain.split(".");
-  var arrLen = splitArr.length;
-
-  if (arrLen > 2) {
-    domain = splitArr[arrLen - 2] + "." + splitArr[arrLen - 1];
-    if (splitArr[arrLen - 1].length == 2 && splitArr[arrLen - 1].length == 2) {
-      domain = splitArr[arrLen - 3] + "." + domain;
+  function extractHostname(url) {
+    var hostname;
+    if (url.indexOf("://") > -1) {
+      hostname = url.split("/")[2];
+    } else {
+      hostname = url.split("/")[0];
     }
+
+    hostname = hostname.split(":")[0];
+    hostname = hostname.split("?")[0];
+
+    return hostname;
   }
-  return domain;
-}
 
-$form.on("submit", function(event) {
-  event.preventDefault();
-  var $star = $('<i class="far fa-star"></i>');
-  $star.attr("aria-hidden", "true");
-  var $title = $("#title").val();
-  var $url = $("#url").val();
-  if (!isUrl($url)) {
-    alert("Invalid URL Formatting.");
-  } else {
-    var $newLi = $("<li>", {
-      class: "listing favfalse"
-    });
-    $newLi.append($star);
-    $newLi.append("<a href=" + $url + " class='titlelink'>" + $title + "</a>");
-    var domain = extractRootDomain($url);
-    var $newUrl = $("<a>", {
-      text: "(" + domain + ")",
-      href: domain,
-      class: "listing__url ml-2"
-    });
+  function extractRootDomain(url) {
+    var domain = extractHostname(url);
+    var splitArr = domain.split(".");
+    var arrLen = splitArr.length;
 
-    $($newLi).append($newUrl);
-    $("ol").append($newLi);
-
-    $form.trigger("reset");
+    if (arrLen > 2) {
+      domain = splitArr[arrLen - 2] + "." + splitArr[arrLen - 1];
+      if (
+        splitArr[arrLen - 1].length == 2 &&
+        splitArr[arrLen - 1].length == 2
+      ) {
+        domain = splitArr[arrLen - 3] + "." + domain;
+      }
+    }
+    return domain;
   }
-});
+  $form = $("form");
+  $form.on("submit", function(event) {
+    var $form = $("form");
+    event.preventDefault();
+    var $star = $("<i>", {
+      class: "far fa-star"
+    });
+    $star.attr("aria-hidden", "true");
+    var $title = $("#title").val();
+    var $url = $("#url").val();
+    if (!isUrl($url)) {
+      alert("Invalid URL Formatting.");
+    } else {
+      var $newLi = $("<li>", {
+        class: "listing favfalse"
+      });
+      $newLi.append($star);
+      $newLi.append(
+        "<a href=" + $url + " class='titlelink'>" + $title + "</a>"
+      );
+      var domain = extractRootDomain($url);
+      var $newUrl = $("<a>", {
+        text: "(" + domain + ")",
+        href: domain,
+        class: "listing__url ml-2"
+      });
 
-$("#submitbutton").click(function(event) {
-  $form.toggle();
-});
+      $($newLi).append($newUrl);
+      $("ol").append($newLi);
 
-$("ol").on("click", "li", function() {
-  $(this).toggleClass("favfalse");
-  $(this)
-    .find("i")
-    .toggleClass("far fas");
-});
+      $form.trigger("reset");
+    }
+  });
 
-$("#favbutton").click(function() {
-  $(".favfalse").toggle();
+  $("#submitbutton").click(function(event) {
+    $form.toggle();
+  });
+
+  $("ol").on("click", "li", function() {
+    $(this).toggleClass("favfalse");
+    $(this)
+      .find("i")
+      .toggleClass("far fas");
+  });
+
+  $("#favbutton").click(function() {
+    $(".favfalse").toggle();
+    $("#favbutton").toggleClass("hidden");
+    $("#allbutton").toggleClass("hidden");
+  });
+
+  $("#allbutton").click(function() {
+    $(".favfalse").toggle();
+    $("#favbutton").toggleClass("hidden");
+    $("#allbutton").toggleClass("hidden");
+  });
 });
