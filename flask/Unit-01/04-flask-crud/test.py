b5673f4c41578f146c4e865bf3978419219e21b8
yang <yueyangyu42@gmail.com>
2018-03-08 09:27:55 -0800

    not ready

--- flask/Unit-01/04-flask-crud/test.py
+++ flask/Unit-01/04-flask-crud/test.py
@@ -2,8 +2,8 @@ from snack import Snack
 from app import app, snack_list
 import unittest
 
-class TestSnackMethods(unittest.TestCase):
 
+class TestSnackMethods(unittest.TestCase):
     def setUp(self):
         snack_list.append(Snack('snickers', 'chocolate'))
         snack_list.append(Snack('skittles', 'candy'))
@@ -33,27 +33,32 @@ class TestSnackMethods(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_creating_snack(self):
-      tester = app.test_client(self)
-      tester.post('/snacks',
-                        data=dict(name="hersheys", kind="chocolate"), follow_redirects = True)
-      self.assertEqual(snack_list[2].id, 3)
-      self.assertEqual(snack_list[2].name, 'hersheys')
-      self.assertEqual(snack_list[2].kind, 'chocolate')
-      self.assertEqual(len(snack_list), 3)
+        tester = app.test_client(self)
+        tester.post(
+            '/snacks',
+            data=dict(name="hersheys", kind="chocolate"),
+            follow_redirects=True)
+        self.assertEqual(snack_list[2].id, 3)
+        self.assertEqual(snack_list[2].name, 'hersheys')
+        self.assertEqual(snack_list[2].kind, 'chocolate')
+        self.assertEqual(len(snack_list), 3)
 
     def test_editing_snack(self):
-      tester = app.test_client(self)
-      tester.post('/snacks/1?_method=PATCH',
-                        data=dict(name="almond_snickers", kind="almonds_and_chocolate"), follow_redirects = True)
-      self.assertEqual(snack_list[0].name, 'almond_snickers')
-      self.assertEqual(snack_list[0].kind, 'almonds_and_chocolate')
-      self.assertEqual(len(snack_list), 2)
+        tester = app.test_client(self)
+        tester.patch(
+            '/snacks/1?_method=PATCH',
+            data=dict(name="almond_snickers", kind="almonds_and_chocolate"),
+            follow_redirects=True)
+        self.assertEqual(snack_list[0].name, 'almond_snickers')
+        self.assertEqual(snack_list[0].kind, 'almonds_and_chocolate')
+        self.assertEqual(len(snack_list), 2)
 
     def test_deleting_snack(self):
-      tester = app.test_client(self)
-      tester.post('/snacks/1?_method=DELETE', follow_redirects = True)
-      tester.post('/snacks/2?_method=DELETE', follow_redirects = True)
-      self.assertEqual(len(snack_list), 0)
+        tester = app.test_client(self)
+        tester.post('/snacks/1?_method=DELETE', follow_redirects=True)
+        tester.post('/snacks/2?_method=DELETE', follow_redirects=True)
+        self.assertEqual(len(snack_list), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
