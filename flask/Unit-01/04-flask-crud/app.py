1749759cd876f9788712b24f6b40c3025432edba
Kelson Warner <kelsonpw35@gmail.com>
2018-03-07 16:48:16 -0800

    initial flask crud commit

--- flask/Unit-01/04-flask-crud/app.py
+++ flask/Unit-01/04-flask-crud/app.py
@@ -1,3 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_modus import Modus
+from car import Car
 
-snack_list = []
+snack_list = [Snack("Snickers", "Candy")]
+
+app = Flask(__name__)
+
+
+@app.route('/')
