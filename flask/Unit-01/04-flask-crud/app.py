34ea91d7408362b47209fe0a984d3c61ebed137c
Leo Ng <leogetsthingsdone@gmail.com>
2018-03-07 21:03:02 -0800

    Elie requested

--- flask/Unit-01/04-flask-crud/app.py
+++ flask/Unit-01/04-flask-crud/app.py
@@ -1,3 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect, request
+from flask_modus import Modus
+from snack import Snack
 
-snack_list = []
+app = Flask(__name__)
+
+snack_list = [Snack("Doritos","Cool Ranch")]
+
+@app.route('/')
+def root():
+    return render_template("/index.html")
+
+@app.route("/snacks/new", methods=["POST"])
+def new():
+    name = request.form.get("name")
+    kind = request.form.get("kind")
+    snack_list.append(Snack(name,kind))
+    return render_template("/new.html", snack=Snack)
+
+@app.route('/snacks', methods=["POST"])
+def new_snack():
+    name = request.form.get("name")
+    kind = request.form.get("kind")
+    snack_list.append(Snack(name,kind))
+    return render_template("index.html", snack=Snack)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
+
+
