e265f8ad1aff82844cfce385b07b3470337b5ad4
Leo Ng <leogetsthingsdone@gmail.com>
2018-03-07 21:39:57 -0800

    another commit

--- flask/Unit-01/04-flask-crud/app.py
+++ flask/Unit-01/04-flask-crud/app.py
@@ -2,20 +2,21 @@ from flask import Flask, render_template, url_for, redirect, request
 from flask_modus import Modus
 from snack import Snack
 
-app = Flask(__name__)
-
 snack_list = [Snack("Doritos","Cool Ranch")]
+
+app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template("/index.html")
+    return redirect(url_for('index'))
 
-@app.route("/snacks/new", methods=["POST"])
+@app.route("/snacks")
+def index():
+    return render_template("index.html", snack=Snack)
+
+@app.route("/snacks/new")
 def new():
-    name = request.form.get("name")
-    kind = request.form.get("kind")
-    snack_list.append(Snack(name,kind))
-    return render_template("/new.html", snack=Snack)
+    return render_template("new.html")
 
 @app.route('/snacks', methods=["POST"])
 def new_snack():
@@ -23,6 +24,22 @@ def new_snack():
     kind = request.form.get("kind")
     snack_list.append(Snack(name,kind))
     return render_template("index.html", snack=Snack)
+
+#########
+# @app.route("/snacks/new")
+# def new():
+#   return render_template("new.html")
+
+# @app.route('/snacks', methods=["POST"])
+# def new_snack():
+#     name = request.form.get("name")
+#     kind = request.form.get("kind")
+#     snack_list.append(Snack(name,kind))
+#     return render_template("index.html", snack=Snack)
+
+#########
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
