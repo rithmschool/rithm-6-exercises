f1b7bbf86567576d1b1880e4ab3d649ec2e84c87
Charmaine Estacio <charmaine.estacio@gmail.com>
2018-03-07 17:32:58 -0800

    updating files

--- flask/Unit-01/04-flask-crud/app.py
+++ flask/Unit-01/04-flask-crud/app.py
@@ -1,9 +1,32 @@
 from flask import Flask, render_template, request, url_for
 from snack import Snack
+from flask_modus import Modus
 
 app = Flask(__name__)
+modus = Modus(app)
 
 snack_list = []
+
+def find_snack():
+    return [snack for snack in snack_list if snack.id == id][0]
+
+@app.route("/")
+def root():
+    return render_template('index.html')
+
+@app.route("/snacks/new")
+def new():
+    return render_template('new.html')
+
+@app.route("/snacks/<int:id>")
+def show():
+    pass
+
+@app.route("/snacks/<int:id>/edit")
+def edit():
+    pass
+
+
 
 if __name__ == 'main':
     app.run(debug=True, port=3000)
