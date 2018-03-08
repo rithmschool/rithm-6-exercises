da55f024af8f3816d11607f45bda541c89f55fbb
Charmaine Estacio <charmaine.estacio@gmail.com>
2018-03-07 16:54:37 -0800

    adding files

--- flask/Unit-01/04-flask-crud/app.py
+++ flask/Unit-01/04-flask-crud/app.py
@@ -1,3 +1,9 @@
 from flask import Flask, render_template, request, url_for
+from snack import Snack
+
+app = Flask(__name__)
 
 snack_list = []
+
+if __name__ == 'main':
+    app.run(debug=True, port=3000)
