ca2a35adb671e0e48ac68b94563fba837caf20fb
Daniel Szabo <dgszabo@gmail.com>
2018-03-07 17:33:06 -0800

    Daniel-04-flask-crud - JUST STARTED

--- flask/Unit-01/04-flask-crud/app.py
+++ flask/Unit-01/04-flask-crud/app.py
@@ -1,3 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+from modus import Modus
+from snack import Snack
 
-snack_list = []
+app = Flask(__name__)
+
+snack_list = [ Snack('chocolate bar', 'chocolate'), Snack('chocolate chip cookie', 'cookie') ]
+modus = Modus(app)
