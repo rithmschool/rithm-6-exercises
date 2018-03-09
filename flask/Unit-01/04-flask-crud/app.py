b2841bf841621ff9176265c99fca743ae862b0ed
Karl <karlsecco@gmail.com>
2018-03-08 23:13:49 -0800

    completing all requirements including bonuses

--- flask/Unit-01/04-flask-crud/app.py
+++ flask/Unit-01/04-flask-crud/app.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, jsonify
 from flask_modus import Modus
 from snack import Snack
-import jinja2
+from threading import Timer
 
 app = Flask(__name__)
-mouds = Modus(app)
+modus = Modus(app)
 
-snacks = [
-    Snack('dark chocolate', 'candy'),
-    Snack('beef jerky', 'protein'),
-    Snack('orange', 'fruit')
-]
+snack_list = []
+
+
+def get_snack(id):
+    return next(snack for snack in snack_list if snack.id == id)
 
 
 @app.route('/')
@@ -23,9 +23,9 @@ def index():
     if request.method == 'POST':
         name = request.form.get('name')
         kind = request.form.get('kind')
-        snacks.append(Snack(name, kind))
+        snack_list.append(Snack(name, kind))
         return redirect(url_for('index'))
-    return render_template('index.html', snacks=snacks)
+    return render_template('index.html', snack_list=snack_list)
 
 
 @app.route('/snacks/new')
@@ -33,22 +33,32 @@ def new():
     return render_template('new.html')
 
 
-@app.route('/snacks/<id>', methods=['GET', 'POST', 'DELETE'])
+@app.route('/snacks/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def show(id):
     snack = get_snack(id)
     if request.method == b'PATCH':
         snack.name = request.form.get('name')
         snack.kind = request.form.get('kind')
         return redirect(url_for('index'))
-    if request.method == 'DELETE':
-        snacks.remove(snack)
-        return redirect(url_for('index'))
+    if request.method == b'DELETE':
+        # ajax refactored delete from index will not pass tests; commented out non-bytes string
+        # if request.method == 'DELETE':
+        snack_list.remove(snack)
+        return jsonify({'message': 'snack deleted'})
+    # redirecting to edit form after adding ajax delete to index.html; commented out, will not pass tests
     return render_template('show.html', snack=snack)
+    # return redirect(url_for('edit', id=snack.id))
 
 
-@app.route('/snacks/<id>/edit')
+@app.route('/snacks/<int:id>/edit')
 def edit(id):
+    snack = get_snack(id)
     return render_template('edit.html', snack=snack)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error.html'), 404
 
 
 if __name__ == '__main__':
--- flask/Unit-01/04-flask-crud/snack.py
+++ flask/Unit-01/04-flask-crud/snack.py
@@ -1,24 +1,27 @@
-import random
+# import random
 
 
 class Snack():
 
-    ids = []
+    id = 1
 
-    def create_id():
-        if len(Snack.ids) < 1800:
-            random_id = f'SNK-{str(random.randint(100, 999))}-{str(random.randint(100, 999))}'
-            if random_id in Snack.ids:
-                return Snack.create_id()
-            else:
-                Snack.ids.append(random_id)
-                return random_id
-        print('STOP EATING ALREADY! Delete snacks to continue.')
+    # the app works fine with random id gen but tests will not pass unless id begins at 1 and remains an int :/
 
-    def get_snack(id):
-        return next(snack for snack in snacks if snack.id == id)
+    # ids = []
+
+    # def create_id():
+    #     if len(Snack.ids) < 1800:
+    #         random_id = f'SNK-{str(random.randint(100, 999))}-{str(random.randint(100, 999))}'
+    #         if random_id in Snack.ids:
+    #             return Snack.create_id()
+    #         else:
+    #             Snack.ids.append(random_id)
+    #             return random_id
+    #     print('STOP EATING ALREADY! Delete snacks to continue.')
 
     def __init__(self, name, kind):
         self.name = name
         self.kind = kind
-        self.id = Snack.create_id()
+        # self.id = Snack.create_id()
+        self.id = Snack.id
+        Snack.id += 1
