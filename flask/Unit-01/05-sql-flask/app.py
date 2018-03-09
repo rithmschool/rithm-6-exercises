a3758be9f3de9ebd8e81e40e5c2b5df85d3fd90a
Miranda Howitt <miranda.howitt@gmail.com>
2018-03-09 14:55:54 -0800

    commit before testing

--- flask/Unit-01/05-sql-flask/app.py
+++ flask/Unit-01/05-sql-flask/app.py
@@ -91,5 +91,5 @@ def page_not_found(e):
     return render_template('404.html')
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port=3000)
+# if __name__ == "__main__":
+#     app.run(debug=True, port=3000)
