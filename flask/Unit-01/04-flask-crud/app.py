9112fb9a355a9fd0061287afbafc6b5d5b66f575
Charmaine Estacio <charmaine.estacio@gmail.com>
2018-03-09 17:42:38 -0800

    update flask 4 files

--- flask/Unit-01/04-flask-crud/app.py
+++ flask/Unit-01/04-flask-crud/app.py
@@ -1,16 +1,28 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_modus import Modus
-from snack import Snack
-
-
-snack_list = []
+# from snack import Snack
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/snacks_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 modus = Modus(app)
 
+snacks = []
 
-def find_snack(id):
-    return [snack for snack in snack_list if snack.id == id][0]
+class Snack(db.Model):
+
+    __tablename__ = 'snacks'
+
+    # DDL
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    kind = db.Column(db.Text)
+
+    def __init__(self, name, kind):
+        self.name = name
+        self.kind = kind
 
 @app.route("/")
 def root():
@@ -19,11 +31,11 @@ def root():
 @app.route("/snacks", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        name = request.form.get("name")
-        kind = request.form.get("kind")
-        snack_list.append(Snack(name, kind))
+        new_snack = Snack(request.form.get('name'), request.form.get('kind'))
+        db.session.add(new_snack)
+        db.session.commit()
         return redirect(url_for('index'))
-    return render_template('index.html', snack_list=snack_list)
+    return render_template('index.html', snacks=Snack.query.all())
 
 @app.route("/snacks/new")
 def new():
@@ -31,26 +43,33 @@ def new():
 
 @app.route("/snacks/<int:id>", methods=["GET", "PATCH", "DELETE"])
 def show(id):
-    snack = find_snack(id)
+    snack = Snack.query.get(id)
     if request.method == b"PATCH":
-        snack.name = request.form.get("name")
-        snack.kind = request.form.get("kind")
+        # updated_snack = Snack(request.form.get('name'), request.form.get('kind'))
+        snack.name = request.form.get('name')
+        snack.kind = request.form.get('kind')
+        db.session.add(snack)
+        db.session.commit()
         return redirect(url_for('index'))
 
     if request.method == b"DELETE":
-        snack_list.remove(snack)
+        db.session.delete(snack)
+        db.session.commit()
         return redirect(url_for('index'))
 
-    return render_template('show.html', snack=snack)
+    return render_template('show.html', snack=Snack.query.get(id))
 
 @app.route("/snacks/<int:id>/edit")
 def edit(id):
-    snack = find_snack(id)
-    return render_template("edit.html", snack=snack)
+    snack = Snack.query.get(id)
+    return render_template("edit.html", snack=Snack.query.get(id))
+
+
+@app.errorhandler(500)
+def internal_error(e):
+    return render_template('500.html'), 500
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
-if __name__ == '__main__':
-    app.run(debug=True, port=3000)
