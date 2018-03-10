1f9ef53dcf4409cf38b8ecbec3439ab04ac36005
Charmaine Estacio <charmaine.estacio@gmail.com>
2018-03-10 09:12:51 -0800

    adding js file

--- flask/Unit-01/05-sql-alchemy-1/static/main.js
+++ flask/Unit-01/05-sql-alchemy-1/static/main.js
@@ -1,18 +1,4 @@
-// $(function() {
-//     $("ul").on("click", "fas fa-trash", function(event) {
-//         event.preventDefault();
-//         var $snackId = parseInt($(event.target).closest('li').attr("id"));
-//         return $.ajax({
-//             method: "DELETE",
-//             url: `/snacks/${$snackId}`
-//         }).then(function() {
-//             $(event.target).closest('li').fadeOut();
-//         });
-//     });
-// })
-
-
-$("#snack-list").on("click", ".far fa-trash-alt", function(event) {
+$("#snack-list").on("click", ".fa-trash-alt", function(event) {
     event.preventDefault();
     var $snackID = parseInt(
         $(this)
