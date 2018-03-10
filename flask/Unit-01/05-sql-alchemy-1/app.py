93ef827ae8d3ee43b6921aa91132954570614df0
Miranda Howitt <miranda.howitt@gmail.com>
2018-03-09 16:11:53 -0800

    commiting all

--- flask/Unit-01/05-sql-alchemy-1/app.py
+++ flask/Unit-01/05-sql-alchemy-1/app.py
@@ -2,13 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_modus import Modus
 
-snack_list = [
-    # Snack("Skyr", "Yogurt"),
-    # Snack("Pretzel Crisps", "Pretzels"),
-    # Snack("M&Ms", "Chocolate"),
-    # Snack("Carrots", "Vegetables"),
-    # Snack("Grapefruit", "Fruit")
-]
+snack_list = []
 
 app = Flask(__name__)
 modus = Modus(app)
@@ -31,7 +25,6 @@ class Snack(db.Model):
     def __init__(self, name, kind):
         self.name = name
         self.kind = kind
-        # self.id = id
 
 
 @app.route("/")
@@ -42,7 +35,6 @@ def root():
 @app.route("/snacks", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        # get some data from a form
         new_snack = Snack(request.form.get("name"), request.form.get("kind"))
         db.session.add(new_snack)
         db.session.commit()
@@ -58,10 +50,8 @@ def new():
 @app.route("/snacks/<int:id>", methods=["GET", "PATCH", "DELETE"])
 def show(id):
     found_snack = Snack.query.get(id)
-    # found_snack = [snack for snack in snack_list if snack.id == id]
-    # if len(found_snack) is 0:
-    #     return render_template('404.html')
-    # found_snack = [snack for snack in snack_list if snack.id == id][0]
+    if found_snack is None:
+         return render_template('404.html')
     if request.method == b"PATCH":
         found_snack.name = request.form.get("name")
         found_snack.kind = request.form.get("kind")
@@ -71,7 +61,6 @@ def show(id):
     elif request.method == b"DELETE":
         db.session.delete(found_snack)
         db.session.commit()
-        # snack_list.remove(found_snack)
         return redirect(url_for('index'))
 
     return render_template("show.html", found_snack=found_snack)
@@ -80,9 +69,8 @@ def show(id):
 @app.route("/snacks/<int:id>/edit")
 def edit(id):
     found_snack = Snack.query.get(id)
-    # if len(found_snack) == 0:
-    #     return render_template('404.html')
-    # found_snack = [snack for snack in snack_list if snack.id == id][0]
+    if found_snack is None:
+         return render_template('404.html')
     return render_template("edit.html", found_snack=found_snack)
 
 
@@ -90,6 +78,3 @@ def edit(id):
 def page_not_found(e):
     return render_template('404.html')
 
-
-# if __name__ == "__main__":
-#     app.run(debug=True, port=3000)
