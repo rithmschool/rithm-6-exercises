49e4107fe8ded9a1177baea6eaffeeaa05e2f780
Zoran Savic <zoransavic@me.com>
2018-03-10 12:17:36 -0800

    Bootcamps started

--- flask/Unit-01/04-flask-crud/app.py
+++ flask/Unit-01/04-flask-crud/app.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_modus import Modus
-from snack import Snack
-
-snack_list = []
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://localhost/05-flask-crud"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 modus = Modus(app)
+db = SQLAlchemy(app)
+
+
+class Snack(db.Model):
+
+    __tablename__ = "snacks"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    kind = db.Column(db.Text)
+
+    def __init__(self, name, kind):
+        self.name = name
+        self.kind = kind
 
 
 @app.route("/")
@@ -13,13 +29,14 @@ def root():
     return redirect(url_for('index'))
 
 
-@app.route('/snacks', methods=['GET', 'POST'])
+@app.route('/snacksz', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         new_snack = Snack(request.form.get('name'), request.form.get('kind'))
-        snack_list.append(new_snack)
+        db.session.add(new_snack)
+        db.session.commit()
         return redirect(url_for("index"))
-    return render_template('index.html', snack_list=snack_list)
+    return render_template('index.html', snack_list=Snack.query.all())
 
 
 @app.route("/snacks/new")
@@ -29,13 +46,16 @@ def new():
 
 @app.route('/snacks/<int:id>', methods=["GET", "PATCH", "DELETE"])
 def show(id):
-    snack = get_snack_by_id(id)
+    snack = Snack.query.get_or_404(id)
     if request.method == b"PATCH":
         snack.name = request.form.get("name")
         snack.kind = request.form.get("kind")
+        db.session.add(snack)
+        db.session.commit()
         return redirect(url_for("show", id=snack.id))
     if request.method == b"DELETE":
-        snack_list.remove(snack)
+        db.session.delete(snack)
+        db.session.commit()
         return redirect(url_for('index'))
     return render_template('show.html', snack=snack)
 
@@ -43,14 +63,12 @@ def show(id):
 @app.route(
     "/snacks/<int:id>/edit", )
 def edit(id):
-    snack = get_snack_by_id(id)
-    return render_template('edit.html', snack=snack)
+    return render_template('edit.html', snack=Snack.query.get(id))
 
 
-def get_snack_by_id(id):
-    selected_snack = [el for el in snack_list if el.id == id][0]
-
-    return selected_snack
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == "__main__":
