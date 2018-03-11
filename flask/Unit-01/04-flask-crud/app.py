14c6a089f77017f90943dcbd15e474caf0282a5d
Alex Bush <alexabush7@gmail.com>
2018-03-11 16:28:09 -0700

    added styling to more views

--- flask/Unit-01/04-flask-crud/app.py
+++ flask/Unit-01/04-flask-crud/app.py
@@ -77,3 +77,7 @@ def edit(id):
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def id_not_found(e):
+    return render_template('500.html'), 500
