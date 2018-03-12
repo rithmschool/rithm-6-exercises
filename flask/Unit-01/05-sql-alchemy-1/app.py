15000430139f9fb1a794ed901861d41c74c76746
spacexengineer <hunter.casbeer@gmail.com>
2018-03-11 17:37:06 -0700

    completed database integration,added requirements, and passing all tests

--- flask/Unit-01/05-sql-alchemy-1/app.py
+++ flask/Unit-01/05-sql-alchemy-1/app.py
@@ -7,9 +7,23 @@ from flask_modus import Modus
 
 app = Flask(__name__)
 modus = Modus(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/computers-db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/snacks-db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+class Snack(db.Model):
+
+    __tablename__ = "snacks"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    kind = db.Column(db.Text)
+
+    def __init__(self, name, kind):
+        self.name = name
+        self.kind = kind
+
 
 # def get_snack(id):
 #     return next(snack for snack in snack_list if snack.id == id)
@@ -23,12 +37,15 @@ def root():
 @app.route('/snacks', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        snack_list.append(
-            Snack(request.form.get('name'),
-                  request.form.get('kind'))
-        )
+        # snack_list.append(
+        #     Snack(request.form.get('name'),
+        #           request.form.get('kind'))
+        # )
+        new_snack = Snack(request.form.get('name'), request.form.get('kind'))
+        db.session.add(new_snack)
+        db.session.commit()
         return redirect(url_for('index'))
-    return render_template('index.html', snack_list=snack_list)
+    return render_template('index.html', snack_list=Snack.query.all())
 
 
 @app.route('/snacks/new')
@@ -39,14 +56,18 @@ def new():
 @app.route('/snacks/<int:id>', methods=["GET", "PATCH", "DELETE"])
 def show(id):
     # found_snack = [snack for snack in snack_list if snack.id == id][0]
-    found_snack = get_snack(id)
+    # found_snack = get_snack(id)
+    found_snack = Snack.query.get(id)
     if request.method == b"PATCH":
         found_snack.name = request.form['name']
         found_snack.kind = request.form['kind']
         # return redirect(url_for('index', found_snack=found_snack))
+        db.session.commit()
         return render_template('show.html', found_snack=found_snack)
     if request.method == b"DELETE":
-        snack_list.remove(found_snack)
+        # snack_list.remove(found_snack)
+        db.session.delete(found_snack)
+        db.session.commit()
         return redirect(url_for('index'))
     return render_template('show.html', found_snack=found_snack)
     # return render_template(url_for('show', found_snack=found_sn))
@@ -54,7 +75,8 @@ def show(id):
 
 @app.route('/snacks/<int:id>/edit')
 def edit(id):
-    found_snack = get_snack(id)
+    # found_snack = get_snack(id)
+    found_snack = Snack.query.get(id)
     return render_template('edit.html', found_snack=found_snack)
 
 
