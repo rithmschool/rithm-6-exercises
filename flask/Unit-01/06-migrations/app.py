219719d074e68979c48888b1c61ec96b7d908f16
Daniel Szabo <dgszabo@gmail.com>
2018-03-12 14:30:55 -0700

    Sunset app - migartion created and basic data table added

---
+++ flask/Unit-01/06-migrations/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, redirect, request, render_template, url_for
+from flask_modus import Modus
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/06-migrations'
+app.config['SQLALCHEMY_ECHO'] = True
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+class Sunset(db.Model):
+    __tablename__ = 'sunsets'
+
+    id = db.Column(db.Integer, primary_key = True)
+    image_url = db.Column(db.Text)
+    caption = db.Column(db.Text)
+    
+@app.route('/')
+def root():
+    return redirect(url_for('index'))
+
+@app.route('/sunsets')
+def index():
+    return render_template('index.html', sunsets = Sunset.query.all())
+
+@app.route('/sunsets/new', methods = ['POST'])
+def new():
+    new_sunset = Sunset(image_url = request.form.get('image_url'), caption = request.form.get('caption'))
+    db.session.add(new_sunset)
+    db.session.commit()
+    return redirect(url_for('index'))
