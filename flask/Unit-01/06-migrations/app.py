a9ff94de178b95f24fbda5aa4355794e4a9ab7be
Charmaine Estacio <charmaine.estacio@gmail.com>
2018-03-12 14:14:44 -0700

    added form

---
+++ flask/Unit-01/06-migrations/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_modus import Modus
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://localhost/06-migrations"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+db = SQLAlchemy(app)
+Migrate = app(db)
+
+class Sunset(db.Model):
+    __tablename__ = "sunsets"
+
+    id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.Text)
+    caption = db.Column(db.Text)
+
+@app.routes("/")
+def root():
+    return redirect(url_for("index"))
+
+@app.routes("/sunsets")
+def index():
+    return render_template("index.html", sunsets=Sunset.query.all())
+
+@app.routes("/sunsets/new", methods=["POST"])
+def new():
+    new_sunset = Sunset(image_url=request.form.get("image_url"), caption=request.form.get("caption"))
+    db.session.add(new_sunset)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
