621b8dda5689905ac8d23dcd18408a8bcf8229a7
Alex Bush <alexabush7@gmail.com>
2018-03-11 19:39:22 -0700

    wrote skeletons for templates

--- flask/Unit-01/05-sql-alchemy-1/app.py
+++ flask/Unit-01/05-sql-alchemy-1/app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, redirect, url_for, request
-from flask_sqlalchemy import flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_modus import Modus
 
 app = Flask(__name__)
 modus = Modus(app)
-app.config['SQLALCHEMY_DATABASE_URL'] = 'postgres:://localhost/flask-sql'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/flask-sql'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 app.url_map.strict_slashes = False
 
 class Bootcamp(db.Model):
+
     __tablename__ = "bootcamps"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -26,8 +27,19 @@ class Bootcamp(db.Model):
     def __repr__(self):
         return f"{self.name} is locationed {self.location}"
 
+def get_bootcamp(id):
+    pass
+
 @app.route('/bootcamps', methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        pass
+    else:
+        bootcamps = Bootcamp.query.all()
+        return render_template('index.html', bootcamps=bootcamps)
+
+@app.route('/bootcamps/new')
+def new():
     pass
 
 @app.route('/bootcamps/<int:id>', methods=["GET", "PATCH", "DELETE"])
