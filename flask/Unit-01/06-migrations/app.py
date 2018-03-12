12f5a2b277693c1dfb6c731b9eaaee1a7aca21aa
Alex Bush <alexabush7@gmail.com>
2018-03-12 14:42:53 -0700

    finished main exercises

--- flask/Unit-01/06-migrations/app.py
+++ flask/Unit-01/06-migrations/app.py
@@ -15,9 +15,10 @@ class Sunset(db.Model):
     __tablename__ = "sunsets"
 
     id = db.Column(db.Integer, primary_key=True)
-    image_url = db.Column(db.Text)
+    image_url = db.Column(db.Text, nullable=False)
     caption = db.Column(db.Text)
     location = db.Column(db.Text)
+    beauty = db.Column(db.Integer)
 
 @app.route('/')
 def root():
