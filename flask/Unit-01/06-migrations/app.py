ff886e04e6ae90e4f77d78f6e369ff8f14483cab
RICHARD C BERBERIAN <humblemind@hotmail.com>
2018-03-12 16:38:23 -0700

    added location

--- flask/Unit-01/06-migrations/app.py
+++ flask/Unit-01/06-migrations/app.py
@@ -6,12 +6,12 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 modus = Modus(app)
 
-app.config['SQLALCHEMY_DATABASE_URL'] = 'postgres://localhost/sunsets-db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/sunsets-db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+Migrate(app, db)
 
 # two endpoints: one to render the `index` page, and one to handle the form submission when a user adds a sunset?
 
@@ -23,10 +23,12 @@ class Sunset(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     image_url = db.Column(db.Text)
     caption = db.Column(db.Text)
+    location = db.Column(db.Text)
 
-    def __init__(self, image_url, caption):
+    def __init__(self, image_url, caption, location):
         self.image_url = image_url
         self.caption = caption
+        self.location = location
 
 
 @app.route('/')
@@ -39,8 +41,9 @@ def index():
     if request.method == 'POST':
         image_url = request.form.get('image_url')
         caption = request.form.get('caption')
-        s = Sunset(image_url, caption)
+        location = request.form.get('location')
+        s = Sunset(image_url, caption, location)
         db.session.add(s)
         db.session.commit()
         return redirect(url_for("index"))
-    return render_template('index.html', Sunset.query.all())
+    return render_template('index.html', sunsets=Sunset.query.all())
