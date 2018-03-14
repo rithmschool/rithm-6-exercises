91687d743cb703c1a645f1c1bf896dcad6fcb0bb
RICHARD C BERBERIAN <humblemind@hotmail.com>
2018-03-13 19:07:33 -0700

    added student table and user templates

--- flask/Unit-01/07-sql-alchemy-2/app.py
+++ flask/Unit-01/07-sql-alchemy-2/app.py
@@ -19,15 +19,30 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.Text)
     last_name = db.Column(db.Text)
+    messages = db.relationship(
+        'Message', backref='user', lazy='dynamic', cascade='all,delete')
 
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
 
 
+class Message(db.Model):
+
+    __tablename__ = 'messages'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __init__(self, content, user_id):
+        self.content = content
+        self.user_id = user_id
+
+
 @app.route('/')
 def root():
-    redirect(url_for('users/index'))
+    return redirect(url_for('index'))
 
 
 @app.route('/users', methods=['GET', 'POST'])
@@ -39,21 +54,21 @@ def index():
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('index'))
-    return render_template('users/index', users=User.query.all())
+    return render_template('users/index.html', users=User.query.all())
 
 
 @app.route('/users/new')
 def new():
-    return render_template('users/new')
+    return render_template('users/new.html')
 
 
-@app.route('/users/<id>/edit')
+@app.route('/users/<int:id>/edit')
 def edit(id):
     user = User.query.get(id)
-    return render_template('users/edit', user=user)
+    return render_template('users/edit.html', user=user)
 
 
-@app.route('/users/<id>', methods=['GET', 'PATCH', 'DELETE'])
+@app.route('/users/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def show(id):
     user = User.query.get(id)
     if request.method == b"PATCH":
@@ -61,9 +76,9 @@ def show(id):
         user.last_name = request.form['last_name']
         db.session.add(user)
         db.session.commit()
-        return redirect('users/index')
+        return redirect(url_for('index'))
     if request.method == b"DELETE":
         db.session.delete(user)
         db.session.commit()
-        return redirect(url_for('users/index'))
-    return render_template('users/show', user=user)
+        return redirect(url_for('index'))
+    return render_template('users/show.html', user=user)
