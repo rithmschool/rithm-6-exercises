fd880a7008357aeb1444c52a8ab300347c682e40
Karl <karlsecco@gmail.com>
2018-03-15 14:18:23 -0700

    initializing files, beginning from current 09-forms submission

---
+++ flask/Unit-02/01-blueprints/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators
+
+
+class UForm(FlaskForm):
+
+    first_name = StringField('First Name', [validators.DataRequired(message='Please enter user\'s first name')])
+    last_name = StringField('Last Name', [validators.DataRequired(message='Please enter user\'s last name')])
+    image_url = StringField('Image URL')
+
+
+class MForm(FlaskForm):
+
+    content = StringField('Content', [validators.DataRequired(message='Please enter a message to display')])
+
+
+class DForm(FlaskForm):
+    pass
