0c59dc1367499d8db9967fd64508ce4c5a804322
Karl <karlsecco@gmail.com>
2018-03-15 13:14:39 -0700

    adding cards w/ message to index

--- flask/Unit-01/09-forms/forms.py
+++ flask/Unit-01/09-forms/forms.py
@@ -6,7 +6,7 @@ class UForm(FlaskForm):
 
     first_name = StringField('First Name', [validators.DataRequired(message='Please enter user\'s first name')])
     last_name = StringField('Last Name', [validators.DataRequired(message='Please enter user\'s last name')])
-    image_url = StringField('Image URL', [validators.URL(message='Please enter a complete URL')])
+    image_url = StringField('Image URL')
 
 
 class MForm(FlaskForm):
