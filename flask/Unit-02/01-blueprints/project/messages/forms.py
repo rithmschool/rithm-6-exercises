f263553de52a578a4dac61dbafcec386f259ca7b
spacexengineer <hunter.casbeer@gmail.com>
2018-03-15 22:53:57 -0700

    changed everything over to use Flask Blueprint layout

--- flask/Unit-02/01-blueprints/project/messages/forms.py
+++ flask/Unit-02/01-blueprints/project/messages/forms.py
@@ -2,12 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, PasswordField, IntegerField, validators
 
 
-class UserForm(FlaskForm):
-    first_name = StringField("First Name", [validators.DataRequired()])
-    last_name = StringField(
-        "Last Name", [validators.DataRequired(), validators.Length(max=20)])
-
-
 class MessageForm(FlaskForm):
     content = StringField("Content", [validators.DataRequired()])
 
