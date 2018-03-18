0880c7faf299bbdd8d4b340ff3300809e63ab981
Sunny <kanwalbirss@gmail.com>
2018-03-17 17:58:36 -0700

    testing completed for sql clhemy 7

--- flask/Unit-01/08-testing/test.py
+++ flask/Unit-01/08-testing/test.py
@@ -1,4 +1,5 @@
-from app import app,db, User, Message
+from IPython import embed
+from app import app, db, User, Message
 from flask_testing import TestCase
 import unittest
 
@@ -9,111 +10,167 @@ class BaseTestCase(TestCase):
         return app
 
     def setUp(self):
+        """ Setting up information in Database"""
+
         db.create_all()
-        user1 = User("Elie", "Schoppik")
-        user2 = User("Tim", "Garcia")
-        user3 = User("Matt", "Lane")
-        db.session.add_all([user1, user2, user3])
-        message1 = Message("Hello Elie!!", 1)
-        message2 = Message("Goodbye Elie!!", 1)
-        message3 = Message("Hello Tim!!", 2)
-        message4 = Message("Goodbye Tim!!", 2)
-        db.session.add_all([message1, message2, message3,message4])
+        user1 = User("Steve", "Jobs", "https://en.wikipedia.org/wiki/Steve_Jobs#/media/File:Steve_Jobs_Headshot_2010-CROP.jpg")
+        user2 = User("Steve", "Wozniak", "https://en.wikipedia.org/wiki/Steve_Wozniak#/media/File:Steve_Wozniak_by_Gage_Skidmore.jpg")
+        user3 = User("Tim", "Cook", "https://en.wikipedia.org/wiki/Tim_Cook#/media/File:Tim_Cook_2009_cropped.jpg")
+        user4 = User("Jony", "Ive", "https://en.wikipedia.org/wiki/Jonathan_Ive#/media/File:Jonathan_Ive_(OTRS).jpg")
+        db.session.add_all([user1, user2, user3, user4])
+        message1 = Message("SJ Apple 1", 1)
+        message2 = Message("SW Apple 2", 2)
+        message3 = Message("TC iPhone 3", 3)
+        message4 = Message("TC iPad 3", 3)
+        message5 = Message("JI iPhone 4", 4)
+        message6 = Message("JI iPad 4", 4)
+        db.session.add_all([message1, message2, message3, message4, message5, message6])
         db.session.commit()
 
     def tearDown(self):
+        """Deleting information from Database """
+
         db.drop_all()
 
-    def test_users_index(self):
+################### Testing Users View Functions #########################
+
+    def test_root_get(self):
+        """Testing root is redirected to index_users"""
+
+        response = self.client.get('/', content_type='html/text', follow_redirects=True)
+        self.assertLess(response.status_code, 400)
+        self.assertIn(b'Steve Jobs', response.data)
+        self.assertIn(b'Steve Wozniak', response.data)
+        self.assertIn(b'Tim Cook', response.data)
+        self.assertIn(b'Jony Ive', response.data)
+
+    def test_index_users_get(self):
+        """Testing users are displayed on home page"""
+
         response = self.client.get('/users', content_type='html/text', follow_redirects=True)
         self.assertLess(response.status_code, 400)
-        self.assertIn(b'Elie Schoppik', response.data)
-        self.assertIn(b'Tim Garcia', response.data)
-        self.assertIn(b'Matt Lane', response.data)
+        self.assertIn(b'Steve Jobs', response.data)
+        self.assertIn(b'Steve Wozniak', response.data)
+        self.assertIn(b'Tim Cook', response.data)
+        self.assertIn(b'Jony Ive', response.data)
 
-    def test_users_show(self):
+    def test_show_users_get(self):
+        """Testing standard response for successful user HTTP requests"""
+
         response = self.client.get('/users/1')
         self.assertEqual(response.status_code, 200)
 
-    def test_users_create(self):
-        response = self.client.post(
-            '/users',
-            data=dict(first_name="Awesome", last_name="Student"),
-            follow_redirects=True
-        )
+        response = self.client.get('/users/2')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Awesome', response.data)
 
-    def test_users_edit(self):
-        response = self.client.get(
-            '/users/1/edit'
-        )
-        self.assertIn(b'Elie', response.data)
-        self.assertIn(b'Schoppik', response.data)
+    def test_index_users_post(self):
+        """Testing new user is created successfully"""
 
-    def test_users_update(self):
-        response = self.client.patch(
-            '/users/1?_method=PATCH',
-            data=dict(first_name="updated", last_name="information"),
-            follow_redirects=True
-        )
-        self.assertIn(b'updated information', response.data)
-        self.assertNotIn(b'Elie Schoppik', response.data)
+        response = self.client.post('/users', data=dict(first_name="Bill", last_name="Gates", image_url=""), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Bill Gates', response.data)
 
-    def test_users_delete(self):
-        response = self.client.delete(
-            '/users/1?_method=DELETE',
-            follow_redirects=True
-        )
-        self.assertNotIn(b'Elie Schoppik', response.data)
+        response = self.client.post('/users', data=dict(first_name="Jude", last_name="Law", image_url=""), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Jude Law', response.data)
 
-    #### TESTS FOR MESSAGES ####
+    def test_edit_users_get(self):
+        """Testing user information is displayed successfully during editing"""
 
-    def test_messages_index(self):
-        response = self.client.get('/users/1/messages', content_type='html/text', follow_redirects=True)
+        response = self.client.get('/users/1/edit')
+        self.assertIn(b'Steve', response.data)
+        self.assertIn(b'Jobs', response.data)
+
+        response = self.client.get('/users/2/edit')
+        self.assertIn(b'Steve', response.data)
+        self.assertIn(b'Wozniak', response.data)
+
+    def test_show_users_patch(self):
+        """Testing user information is updated successfully"""
+
+        response = self.client.patch('/users/1?_method=PATCH', data=dict(first_name="Jackie", last_name="Chan", image_url=""), follow_redirects=True)
+        self.assertIn(b'Jackie Chan', response.data)
+        self.assertNotIn(b'Steve Jobs', response.data)
+
+        response = self.client.patch('/users/3?_method=PATCH', data=dict(first_name="Donald", last_name="Duck", image_url=""), follow_redirects=True)
+        self.assertIn(b'Donald Duck', response.data)
+        self.assertNotIn(b'Tim Cook', response.data)
+
+    def test_show_users_delete(self):
+        """Testing user information is deleted successfully"""
+
+        response = self.client.delete('/users/2?_method=DELETE', follow_redirects=True)
+        self.assertNotIn(b'Steve Wozniak', response.data)
+
+        response = self.client.delete('/users/3?_method=DELETE', follow_redirects=True)
+        self.assertNotIn(b'Tim Cook', response.data)
+
+
+################### Testing Message View Functions #########################
+
+    def test_index_messages_get(self):
+        """Testing messages are displayed for corresponding user"""
+
+        response = self.client.get('/users/3/messages', content_type='html/text', follow_redirects=True)
         self.assertLess(response.status_code, 400)
-        self.assertIn(b'Hello Elie!!', response.data)
-        self.assertIn(b'Goodbye Elie!!', response.data)
+        self.assertIn(b'TC iPhone 3', response.data)
+        self.assertIn(b'TC iPad 3', response.data)
 
-    def test_messages_show(self):
-        response = self.client.get('/users/1/messages/1')
+        response = self.client.get('/users/4/messages', content_type='html/text', follow_redirects=True)
+        self.assertLess(response.status_code, 400)
+        self.assertIn(b'JI iPhone 4', response.data)
+        self.assertIn(b'JI iPad 4', response.data)
+
+    def test_show_messages_get(self):
+        """Testing standard response for successful message HTTP requests"""
+
+        response = self.client.get('/users/3/messages/1')
         self.assertEqual(response.status_code, 200)
 
-    def test_messages_create(self):
-        response = self.client.post(
-            '/users/1/messages',
-            data=dict(content="Hi Matt!!", user_id=3),
-            follow_redirects=True
-        )
+        response = self.client.get('/users/4/messages/2')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Hi Matt!!', response.data)
 
-    def test_messages_edit(self):
-        response = self.client.get(
-            '/users/1/messages/1/edit'
-        )
-        self.assertIn(b'Hello Elie!!', response.data)
+    def test_index_messages_post(self):
+        """Testing new message is created successfully"""
 
-        response = self.client.get(
-            '/users/2/messages/4/edit'
-        )
-        self.assertIn(b'Goodbye Tim!!', response.data)
+        response = self.client.post('/users/4/messages', data=dict(content="JI iWatch 5", user_id=4), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'JI iWatch 5', response.data)
 
-    def test_messages_update(self):
-        response = self.client.patch(
-            '/users/1/messages/1?_method=PATCH',
-            data=dict(content="Welcome Back Elie!"),
-            follow_redirects=True
-        )
-        self.assertIn(b'Welcome Back Elie!', response.data)
+    def test_edit_messages_get(self):
+        """Testing message content is displayed successfully during editing"""
 
-    def test_messages_delete(self):
-        response = self.client.delete(
-            '/users/1/messages/1?_method=DELETE',
-            follow_redirects=True
-        )
-        self.assertNotIn(b'Hello Elie!!', response.data)
+        response = self.client.get('/users/1/messages/1/edit')
+        self.assertIn(b'SJ Apple 1', response.data)
 
+        response = self.client.get('/users/1/messages/2/edit')
+        self.assertIn(b'SW Apple 2', response.data)
+
+    def test_show_messages_patch(self):
+        """Testing messages are updated successfully"""
+
+        response = self.client.patch('/users/1/messages/1?_method=PATCH', data=dict(content="NEXT Computers"), follow_redirects=True)
+        self.assertIn(b'NEXT Computers', response.data)
+
+        response = self.client.patch('/users/2/messages/2?_method=PATCH', data=dict(content="Retired Hacker"), follow_redirects=True)
+        self.assertIn(b'Retired Hacker', response.data)
+
+    def test_show_messages_delete(self):
+        """Testing messages are deleted successfully"""
+
+        response = self.client.delete('/users/1/messages/1?_method=DELETE', follow_redirects=True)
+        self.assertNotIn(b'SJ Apple 1', response.data)
+
+        response = self.client.delete('/users/2/messages/1?_method=DELETE', follow_redirects=True)
+        self.assertNotIn(b'SW Apple 2', response.data)
+
+################### Testing 404 Functions #########################
+
+    def test_404_reponse(self):
+        """Testing 404 not found response for invalid url"""
+
+        response = self.client.get('/users/junk', follow_redirects=True)
+        self.assertEqual(response.status_code, 404)
 
 if __name__ == '__main__':
     unittest.main()
