9e7651cb7040f461b9872d374ac9d2de865084c1
spacexengineer <hunter.casbeer@gmail.com>
2018-03-18 19:41:26 -0700

    add table for tags and join table for message_tags

--- flask/Unit-02/02-many-to-many/project/models.py
+++ flask/Unit-02/02-many-to-many/project/models.py
@@ -1,4 +1,5 @@
-from project import db # project refers to __init__.py
+from project import db  # project refers to __init__.py
+
 
 class User(db.Model):
 
@@ -14,6 +15,12 @@ class User(db.Model):
         self.last_name = last_name
 
 
+MessageTag = db.Table('message_tags',
+                      db.Column('id', db.Integer, primary_key=True),
+                      db.Column('message_id', db.Integer, db.ForeignKey('messages.id', ondelete="cascade")),
+                      db.Column('tag_id', db.Integer, db.ForeignKey('tags.id', ondelete="cascade")))
+
+
 class Message(db.Model):
 
     __tablename__ = "messages"
@@ -21,7 +28,21 @@ class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text)
     message_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    tags = db.relationship("Tag",
+                           secondary=MessageTag,
+                           backref=db.backref('messages'))
 
     def __init__(self, content, message_id):
         self.content = content
         self.message_id = message_id
+
+
+class Tag(db.Model):
+
+    __tablename__ = "tags"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+
+    def __init__(self, name):
+        self.name = name
