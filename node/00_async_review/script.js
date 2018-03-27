5e9ebdd32e15e789f86ec20db9824c54d7e3ca20
Kelson Warner <kelsonpw35@gmail.com>
2018-03-27 09:17:35 -0700

    working solution determined

--- node/00_async_review/script.js
+++ node/00_async_review/script.js
@@ -10,33 +10,65 @@ async function openingCrawl() {
   return res;
 }
 
-async function movieName() {
-  let films = await $.getJSON(`${BASE_URL}/films`);
-  let results = films['results'];
-  let names = [];
-  results.forEach(function(film) {
-    names.push(film['title']);
-  });
-  return names;
-}
+// async function movieName() {
+//   let films = await $.getJSON(`${BASE_URL}/films`);
+//   let results = films['results'];
+//   let names = [];
+//   results.forEach(function(film) {
+//     names.push(film['title']);
+//   });
+//   return names;
+// }
 
-async function planets() {
-  let films = await $.getJSON(`${BASE_URL}/films`);
-  let results = films['results'];
-  res = [];
-  results.forEach(function(film) {});
-  return res;
-}
+// async function planets() {
+//   let films = await $.getJSON(`${BASE_URL}/films`);
+//   let results = films['results'];
+//   res = [];
+//   results.forEach(function(film) {});
+//   return res;
+// }
 
-$.getJSON(`${BASE_URL}/films`).then(result => {
-  const processed = result.results.map(film => {
-    const { opening_crawl: crawl, title, planets } = film;
-    return { crawl, title, planets };
+// $.getJSON(`${BASE_URL}/films`).then(result => {
+//   const processed = result.results.map(film => {
+//     const { opening_crawl: crawl, title, planets } = film;
+//     return { crawl, title, planets };
+//   });
+// });
+// $.getJSON('https://swapi.co/api/films').then(res => {
+//   const processed = res.data.results.map(film => {
+//     const { opening_crawl: crawl, title, planets } = film;
+//     return { crawl, title, planets };
+//   });
+// });
+// async function movies() =
+// $.getJSON('https://swapi.co/api/films').then(res => {
+//   var processed = res.data.results.map(async film => {
+//     var { opening_crawl: crawl, title, planets } = film;
+//     return { crawl, title, planets };
+//   }).then(function(data) { console.log(data)});
+// });
+
+$.getJSON(`https://swapi.co/api/films`)
+  .then(data => {
+    let films = [];
+
+    data.results.forEach(function(film) {
+      let obj = {};
+      obj.title = film.title;
+      obj.opening_crawl = film.opening_crawl;
+      let planetsArray = Array.from(film.planets, p => $.getJSON(`${p}`));
+      Promise.all(planetsArray).then(function(planets) {
+        let thisFilmsPlanetNames = [];
+        for (var key in planets) {
+          planetsArray.push(planets[key].name);
+        }
+        obj.planets = planetsArray;
+        films.push(obj);
+      });
+    });
+
+    return films;
+  })
+  .then(films => {
+    console.log(films);
   });
-});
-$.getJSON('https://swapi.co/api/films').then(res => {
-  const processed = res.data.results.map(film => {
-    const { opening_crawl: crawl, title, planets } = film;
-    return { crawl, title, planets };
-  });
-});
