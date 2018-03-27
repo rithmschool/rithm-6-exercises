503b1b5d7f8def3e8d24fc80a2b329d5808495af
Kelson Warner <kelsonpw35@gmail.com>
2018-03-26 18:16:13 -0700

    still working

--- node/00_async_review/script.js
+++ node/00_async_review/script.js
@@ -1,9 +1,28 @@
 const BASE_URL = 'https://swapi.co/api';
-let id = 1;
+
 async function openingCrawl() {
   let films = await $.getJSON(`${BASE_URL}/films`);
   let results = films['results'];
+  res = [];
   results.forEach(function(film) {
-    console.log(film['opening_crawl']);
+    res.push(film['opening_crawl']);
+  });
+  return res;
+}
+
+async function movieName() {
+  let films = await $.getJSON(`${BASE_URL}/films`);
+  let results = films['results'];
+  let names = [];
+  results.forEach(function(film) {
+    names.push(film['title']);
+  });
+  return names;
+}
+
+async function planets() {
+  let planetList = [];
+  $.getJSON(`${BASE_URL}/films`).then(function(data) {
+    return data.results.planets;
   });
 }
