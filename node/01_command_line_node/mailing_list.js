4030ccb4f5a57ebb372394c2aaadde9806a70902
Daniel Szabo <dgszabo@gmail.com>
2018-03-27 17:39:08 -0700

    Daniel 01 command line node - Part 2 completed

---
+++ node/01_command_line_node/mailing_list.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const db = require("./data.json")
+
+let targets = db.filter(el => ['San Francisco', 'Seattle', 'Portland'].includes(el.city));
+let potentials = db.filter(el => ['California', 'Washington', 'Oregon'].includes(el.city) && !['San Francisco', 'Seattle', 'Portland'].includes(el.state))
+
+targets.forEach(cand => {
+    let email_text = `Hello ${cand.firstName},\n\nI saw your experience at ${cand.company} and thought you would be a great fit for us here at LinkedList. Let me know if you're interested in getting coffee or whatever recruiters say...\n\nBest,\n\nRandy Random\nLinkedList`
+    fs.writeFile(`./emails/${cand.email}.txt`, email_text, function(err) {
+        if(err) {
+            console.log('Something went wrong!');
+        } else {
+            console.log(`${cand.firstName} ${cand.lastName}'s file was successfully created!`)
+        }
+    });
+});
+
+potentials.forEach(cand => {
+    fs.appendFile(`./potentials.txt`, `${cand.firstName} ${cand.lastName}, ${cand.email}, ${cand.company}\n`, function(err) {
+        if(err) {
+            console.log('Something went wrong!');
+        } else {
+            console.log(`${cand.firstName} ${cand.lastName}'s entry was added!`)
+        }
+    });
+});
+
+// WORKING READ-IN
+// fs.readFile('./data.json', function(err, res) {
+//     let db = [...JSON.parse(res)];
+//     let targets = db.filter(el => ['San Francisco', 'Seattle', 'Portland'].includes(el.city));
+//     let potentials = db.filter(el => ['San Francisco', 'Seattle', 'Portland'].includes(el.city))
+// });
