8383abb6a244f918a560d204542a4e0964b4a77c
Miranda Howitt <miranda.howitt@gmail.com>
2018-03-27 18:07:13 -0700

    finished hmwk

--- node/00_async_review/script.js
+++ node/00_async_review/script.js
@@ -24,9 +24,10 @@
           for (var key in planets) {
             filmPlanets.push(planets[key].name);
           }
-          objStar.planets = filmPlanets;
-          filmsInfo.push(objStar);
+          return filmPlanets;
         });
+        objStar.planets = filmPlanets;
+        filmsInfo.push(objStar);
       });
 
       return filmsInfo;
@@ -36,37 +37,27 @@
     });
 });
 
+// function queryPlanets() {
+//   return new Promise(resolve, reject) => {
+//     return $.getJSON('hhtps://swapi.co/api/film').then(query1Res => {
+//       const films = Promise.all(query1Res.results.map(film => { //map over creating a new array with objects
+//         return new Promise((resolve, reject) => {
+//           const processedFilm = {};
+//           processedFilm.title = film.title;
+//           processedFilm.opening_crawl = film.opening_crawl;
+//           Promise.all(
+//             film.planets.map(planetURL => {
+//             return $.getJSON(planetURL)
+//             })
+//           ).then(planetObjs => {
+//             const newPlanets = planetObjs.map(p => p.name);
+//             processedFilm.planets = newPlanets;
+//             return resolve(processedFilm)
+//           }); // we got back an an array of objects but we only want an array of strings(name of planets) so we map over the objects to get the names of planets
+//         })
+//       })
+//     ).then(result => resolve(result));
 
-
-
-
-
-
-
-
-
-
-function queryPlanets() {
-  return new Promise(resolve, reject) => {
-    $.getJSON('hhtps://swapi.co/api/film').then(query1Res => {
-      const films = Promise.all(query1Res.results.map(film => { //map over creating a new array with objects
-        return new Promise((resolve, reject) => {
-          const processedFilm = {};
-          processedFilm.title = film.title;
-          processedFilm.opening_crawl = film.opening_crawl;
-          Promise.all(
-            film.planets.map(planetURL => {
-            return $.getJSON(planetURL)
-            })
-          ).then(planetObjs => {
-            const newPlanets = planetObjs.map(p => p.name);
-            processedFilm.planets = newPlanets;
-            return resolve(processedFilm)
-          }); // we got back an an array of objects but we only want an array of strings(name of planets) so we map over the objects to get the names of planets
-        })
-      })
-    ).then(result => resolve(result));
-
-    })
-  }
-}
+//     })
+//   }
+// }
