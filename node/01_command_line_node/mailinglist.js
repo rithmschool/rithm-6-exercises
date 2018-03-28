038bf9f305fe4e363d6fe368ed9a46402e00b20f
Kelson Warner <kelsonpw35@gmail.com>
2018-03-27 18:44:33 -0700

    fixed bug, I believe this is correct now! Please review

--- node/01_command_line_node/mailinglist.js
+++ node/01_command_line_node/mailinglist.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const data = require('./data.json');
 
-let cities = {
+const cities = {
   'San Francisco': 1,
   Seattle: 1,
   Portland: 1
 };
-let states = {
+const states = {
   California: 1,
   Washington: 1,
   Oregon: 1
@@ -31,7 +31,8 @@ for (var i = 0; i < data.length; i++) {
     fs.writeFile(`./emails/${data[i].email}.txt`, email, err => {
       if (err) throw err;
     });
-  } else if (states[state] && (!cities[city] || city === null)) {
+    // } else if (states[state] && (!cities[city] || city === null)) {
+  } else if (states[state] && !cities[city] && city !== null) {
     var potential = `Name: ${data[i].firstName} ${data[i].lastName} Email: ${
       data[i].email
     } Company: ${data[i].company}\n`;
