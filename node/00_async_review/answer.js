5f27f880a1e8892541fe55b12b86b87c46045bc5
Paula Goyanes <goyanespaula@gmail.com>
2018-03-29 16:22:35 -0700

    complete

---
+++ node/00_async_review/answer.js
@@ -0,0 +1,120 @@
+// HOMEWORK PART 1
+// My new answer:
+function queryPlanetsPart1() {
+  return new Promise((resolve, reject) => {
+    return $.getJSON("https://swapi.co/api/films").then(query1Res => {
+      Promise.all(
+        query1Res.results.map(film => {
+          return new Promise((resolve, reject) => {
+            const processedFilm = {};
+            processedFilm.title = film.title;
+            processedFilm.opening_crawl = film.opening_crawl;
+            Promise.all(
+              film.planets.map(planetURL => $.getJSON(planetURL))
+            ).then(planetObjs => {
+              const newPlanets = planetObjs.map(p => p.name);
+              processedFilm.planets = newPlanets;
+              return resolve(processedFilm);
+            });
+          });
+        })
+      ).then(result => resolve(result));
+    });
+  });
+}
+
+arr = [];
+
+setTimeout(function() {
+  resolve(`Hi`);
+}, timeToResolve);
+
+function promiseAll(promisesArr) {
+  for (let i = 0; i < promisesArr.length; i++) {}
+}
+
+// HOMEWORK PART 2 (bonus)
+
+// NOTES ON PART 1
+// My original answer
+$.getJSON("https://swapi.co/api/films")
+  .then(data => {
+    return data.results.map(function(movie) {
+      let obj = {};
+      obj.title = movie.title;
+      obj.crawl = movie.opening_crawl;
+      let thisFilmsPlanets = movie.planets.map(url => $.getJSON(url));
+      Promise.all(thisFilmsPlanets).then(planets => {
+        obj.planets = planets.map(p => p.name);
+      });
+      return obj;
+    });
+  })
+  .then(filmsInfo => console.log(filmsInfo));
+
+// Michael's longer answer explained:
+
+function queryPlanetsPart1() {
+  // this function will return a promise (so it's .then()able)
+  return new Promise((resolve, reject) => {
+    // making an AJAX call to SW API
+    return $.getJSON("https://swapi.co/api/films").then(query1Res => {
+      // doing a bunch of ASYNC stuff in here
+      Promise.all(
+        // for the results (films) of the query response ojbect
+        query1Res.results.map(film => {
+          // we're doing a bunch more async stuff on each
+          return new Promise((resolve, reject) => {
+            // result object here
+            const processedFilm = {};
+            processedFilm.title = film.title;
+            processedFilm.opening_crawl = film.opening_crawl;
+            // we have properties on this object that are async (because we need to query for them)
+            Promise.all(
+              film.planets.map(planetURL => $.getJSON(planetURL))
+            ).then(planetObjs => {
+              const newPlanets = planetObjs.map(p => p.name);
+              processedFilm.planets = newPlanets;
+              // resolve the nested promise (map callback)
+              return resolve(processedFilm);
+            });
+          });
+        })
+      ).then(result => resolve(result)); // resolve outer promise
+    });
+  });
+}
+
+queryPlanetsPart1().then(res => {
+  console.log(res[0].planets[0]);
+});
+
+// If getting planets was straightforward
+function queryPlanets() {
+  // if you need more control over a function, roll your own promise
+  return new Promise((resolve, reject) => {
+    // a promise itself
+    return $.getJSON("https://swapi.co/api/films").then(query1Res => {
+      const films = query1Res.results.map(film => {
+        const processedFilm = {};
+        processedFilm.title = film.title;
+        processedFilm.opening_crawl = film.opening_crawl;
+        processedFilm.planets = film.planets;
+        return processedFilm;
+      });
+      // this return statement is the return statement for the whole function
+      return resolve(films);
+    });
+  });
+}
+
+// Michael's short answer:
+
+axios.get("https://swapi.co/api/films").then(res => {
+  const processed = res.data.results.map(film => {
+    const { opening_crawl: crawl, title, planets } = film;
+    Promise.all(
+      planets.map(planet => axios.get(planet).then(data => data.data.name))
+    ).then(planetNames => console.log({ crawl, title, planetNames }));
+  });
+});
