e8d546340cc5b1b98ddd089d4f65f8e59acd36ca
Alex Bush <alexabush7@gmail.com>
2018-03-28 17:40:10 -0700

    made minor changes

---
+++ node/02_express_intro/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+const PORT = 3000;
+
+function mean(nums) {
+  let len = nums.length;
+  return nums.reduce((a, b) => a + b) / len;
+}
+
+app.get('/mean', (request, response, next) => {
+  if (!request.query.nums) {
+    return next(new Error('Please enter some data'));
+  } else if (request.query.nums.length < 1) {
+    return next(new Error('Please enter some numbers'));
+  } else if (request.query.nums[request.query.nums.length - 1] === ',') {
+    return next(new Error('Please remove the trailing comma, thanks,'));
+  }
+  let numsStr = request.query.nums.split(',');
+  let isNumberError = false;
+  numsStr.forEach(num => {
+    if (isNaN(num)) isNumberError = true;
+  });
+  if (isNumberError)
+    return next(new Error('There was a problem with one of your numbers'));
+  let numNums = numsStr.map(num => +num);
+  let average = mean(numNums);
+  let numStr = numNums.join(', ');
+  fs.appendFileSync(`./results.txt`, numStr, function(err) {
+    if (err) {
+      return next(new Error('There was an error saving the value'));
+    }
+  }
+
+  return response.send(`The average of ${numStr} is ${average}`);
+});
+
+app.get('/median', (request, response, next) => {
+  return response.send(request.phrase);
+});
+
+app.get('/mode', (request, response, next) => {
+  return response.send(request.phrase);
+});
+
+app.use((err, req, res, next) => {
+  return res.status(400).send(err.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`app is listening on ${PORT}`);
+});
