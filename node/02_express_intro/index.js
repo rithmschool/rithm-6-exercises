a706c251d05ead38da3e91038e0462e927931987
Charmaine Estacio <charmaine.estacio@gmail.com>
2018-03-28 18:01:41 -0700

    cleaned up routes and separated math functions

--- node/02_express_intro/index.js
+++ node/02_express_intro/index.js
@@ -7,58 +7,22 @@ let result;
 let input;
 let printedResult;
 
-// /mean?input=stuff     -->   request.query = { input: 'stuff' }
-app.get(`/mean`, (request, response, next) => {
-    if (request.query.nums.length === 0) {
-        console.log('sorry, no numbers');
-        return next();
-    }
-    input = request.query.nums.split(',').map(Number);
-
+function doMean() {
     total = input.reduce((a, b) => a + b, 0);
     // for (let i = 0; i < input.length; i++) {
     //     total += input[i];
     // }
     result = total / input.length;
-    printedResult = `The median of ${input} is: ${result}`;
+}
 
-    fs.appendFile('./results.txt', `${printedResult}\n`, (err) => {
-        if (err) throw err;
-        console.log(`${printedResult} was appended to the file!`)
-    })
-
-    return response.send(printedResult)
-})
-
-app.get(`/median`, (request, response, next) => {
-    if (request.query.nums.length === 0) {
-        console.log('sorry, no numbers');
-        return next();
-    }
-    input = request.query.nums.split(',').map(Number);
-
+function doMedian() {
     let half = Math.floor(input.length / 2);
     input.sort((a, b) => a - b)
     if (input.length % 2 === 0) result = input[half];
     else result = (input[half] + input[half] + 1) / 2;
+}
 
-    printedResult = `The median of ${input} is: ${result}`;
-
-    fs.appendFile('./results.txt', `${printedResult}\n`, (err) => {
-        if (err) throw err;
-        console.log(`${printedResult} was appended to the file!`)
-    })
-
-    return response.send(printedResult)
-})
-
-app.get(`/mode`, (request, response, next) => {
-    if (request.query.nums.length === 0) {
-        console.log('sorry, no numbers');
-        return next();
-    }
-    input = request.query.nums.split(',').map(Number);
-
+function doMode() {
     let counter = {};
     for (let i = 0; i < input.length; i++) {
         if (input[i] in counter) counter[input[i]]++;
@@ -73,13 +37,51 @@ app.get(`/mode`, (request, response, next) => {
         }
     }
     result = value;
-    printedResult = `The median of ${input} is: ${result}`;
+}
 
+// /mean?input=stuff     -->   request.query = { input: 'stuff' }
+app.get(`/mean`, (request, response, next) => {
+    if (request.query.nums.length === 0) {
+        console.log('sorry, no numbers');
+        return next();
+    }
+    input = request.query.nums.split(',').map(Number);
+    doMean(input);
+    printedResult = `The mean of ${input} is: ${result}`;
     fs.appendFile('./results.txt', `${printedResult}\n`, (err) => {
         if (err) throw err;
         console.log(`${printedResult} was appended to the file!`)
     })
+    return response.send(printedResult)
+})
 
+app.get(`/median`, (request, response, next) => {
+    if (request.query.nums.length === 0) {
+        console.log('sorry, no numbers');
+        return next();
+    }
+    input = request.query.nums.split(',').map(Number);
+    doMedian(input);
+    printedResult = `The median of ${input} is: ${result}`;
+    fs.appendFile('./results.txt', `${printedResult}\n`, (err) => {
+        if (err) throw err;
+        console.log(`${printedResult} was appended to the file!`)
+    })
+    return response.send(printedResult)
+})
+
+app.get(`/mode`, (request, response, next) => {
+    if (request.query.nums.length === 0) {
+        console.log('sorry, no numbers');
+        return next();
+    }
+    input = request.query.nums.split(',').map(Number);
+    doMode(input);
+    printedResult = `The mode of ${input} is: ${result}`;
+    fs.appendFile('./results.txt', `${printedResult}\n`, (err) => {
+        if (err) throw err;
+        console.log(`${printedResult} was appended to the file!`)
+    })
     return response.send(printedResult)
 })
 
