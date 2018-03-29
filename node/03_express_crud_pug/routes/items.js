e49c35f702fb6ccf4bfb2f2a5b8845ee8291a828
Daniel Szabo <dgszabo@gmail.com>
2018-03-29 16:45:53 -0700

    03 express crud pug - basic app requirements completed

--- node/03_express_crud_pug/routes/items.js
+++ node/03_express_crud_pug/routes/items.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-let items = ['butter', 'milk', 'eggs', 'bread'];
+let items = [];
 
 router.get('/', (req, res, next) => {
     return res.render('index', {items})
@@ -12,7 +12,9 @@ router.get('/new', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    
+    console.log(req.body)
+    items.push({ item: req.body.inputItem, price: req.body.inputPrice})
+    return res.redirect('/items')
 });
 
 module.exports = router;
