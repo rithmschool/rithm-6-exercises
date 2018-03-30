2e2eff389ed5f5bda8572e1815f62bbe73f31ae0
Mark Suzuki <markdsuzuki@gmail.com>
2018-03-29 21:44:26 -0700

    routes and handler folders created

--- node/03_express_crud_pug/routes/items.js
+++ node/03_express_crud_pug/routes/items.js
@@ -1,2 +1,19 @@
 const express = require("express");
-const routes = express.Router();
+const router = express.Router();
+const shoppingCartHandler = require("../handlers/items");
+
+router.get("/items", shoppingCartHandler.homePage);
+
+router.get("/items/new", shoppingCartHandler.CreateItemForm);
+
+router.post("/items", shoppingCartHandler.CreateItem);
+
+router.get("/items/:id", shoppingCartHandler.showItem);
+
+router.get("/items/:id/edit", shoppingCartHandler.UpdateForm);
+
+router.patch("/items/:id", shoppingCartHandler.UpdateItem);
+
+router.delete("/items/:id", shoppingCartHandler.deleteItem);
+
+module.exports = router;
