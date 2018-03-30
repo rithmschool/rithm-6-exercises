246daab5792b29b330cf0155f130bd31b04364ef
Karl <karlsecco@gmail.com>
2018-03-30 15:16:10 -0700

    refactoring routes

--- node/03_express_crud_pug/routes/index.js
+++ node/03_express_crud_pug/routes/index.js
@@ -10,11 +10,20 @@ const {
   renderEditForm
 } = require('../handlers/index');
 
-router.get('/items', renderItems);
-router.post('/items', createItem);
-router.patch('/items/:id', editItem);
-router.delete('/items/:id', deleteItem);
-router.get('/items/new', renderNewForm);
-router.get('/items/:id/edit', renderEditForm);
+router.route('/items')
+  .get(renderItems)
+  .post(createItem);
+router.route('/items/new').get(renderNewForm);
+router.route('/items/:id')
+  .patch(editItem)
+  .delete(deleteItem);
+router.route('/items/:id/edit').get(renderEditForm);
+
+// router.get('/items', renderItems);
+// router.post('/items', createItem);
+// router.patch('/items/:id', editItem);
+// router.delete('/items/:id', deleteItem);
+// router.get('/items/new', renderNewForm);
+// router.get('/items/:id/edit', renderEditForm);
 
 module.exports = router;
