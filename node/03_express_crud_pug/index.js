070d82562921f92b960b81562a38bb4d0ac2d596
Charmaine Estacio <charmaine.estacio@gmail.com>
2018-03-29 19:14:13 -0700

    completed hw plus bonuses, need to style & refactor using router

--- node/03_express_crud_pug/index.js
+++ node/03_express_crud_pug/index.js
@@ -4,32 +4,59 @@ const morgan = require("morgan");
 const methodOverride = require("method-override");
 
 const app = express();
-const items = [];
 
 app.set("view engine", "pug");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(methodOverride("_method"));
 
-app.get("/", function(req, res, next) {
+const items = [];
+let id = 1;
+
+app.get("/", (req, res, next) => {
     return res.redirect("/items");
 })
 
-app.get("/items", function(req, res, next) {
+app.get("/items", (req, res, next) => {
     return res.render("index", { items });
 });
 
-app.post("/items", function(req, res, next) {
-    items.push(req.body.item);
-    console.log(items);
+app.post("/items", (req, res, next) => {
+    const itemProfile = { id, ...req.body }
+    items.push(itemProfile)
+    id++;
     return res.redirect("/items")
 })
 
-app.get("/items/new", function(req, res, next) {
-    return res.render("new-item")
-
+app.get("/items/new", (req, res, next) => {
+    return res.render("new")
 })
-app.use(function(req, res, next) {
-    // res.status = 500
+
+app.get("/items/:id", (req, res, next) => {
+    const item = items.find(element => element.id === Number(req.params.id));
+    console.log(item);
+    return res.render("show", { item })
+})
+
+
+app.get("/items/:id/edit", (req, res, next) => {
+    const item = items.find(element => element.id === Number(req.params.id));
+    return res.render("edit", { item })
+})
+
+app.patch("/items/:id", (req, res, next) => {
+    const item = items.find(element => element.id === Number(req.params.id));
+    item.item = req.body.item;
+    return res.redirect("/items")
+})
+
+app.delete("/items/:id", (req, res, next) => {
+    const itemIndex = items.findIndex(element => element.id === Number(req.params.id));
+    items.splice(itemIndex, 1);
+    return res.redirect("/items")
+})
+
+app.use((req, res, next) => {
     return res.send("Error page", 500)
 })
 
--- node/03_express_crud_pug/routes/items.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const itemHandlers = require("../handlers/items");
-
-router.get("/", itemHandlers.getItems);
-router.get("/items", )
