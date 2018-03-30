7ddc4fc26120a76a2867e42b0fee0deb5ccd1f94
Miranda Howitt <miranda.howitt@gmail.com>
2018-03-30 12:49:11 -0700

    more style

--- node/03_express_crud_pug/index.js
+++ node/03_express_crud_pug/index.js
@@ -7,7 +7,7 @@ const app = express();
 const itemsRoutes = require("./routes/items");
 
 app.set("view engine", "pug");
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public/style.css"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(methodOverride("_method"));
