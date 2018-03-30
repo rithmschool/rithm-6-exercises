a76f3645608bda05990cfeeb5247692b213daac9
Daniel Szabo <dgszabo@gmail.com>
2018-03-29 18:24:09 -0700

    03 express crud pug - edit and delete functionality added

--- node/03_express_crud_pug/app.js
+++ node/03_express_crud_pug/app.js
@@ -4,15 +4,21 @@ const PORT = 3001;
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const itemRoutes = require('./routes/items');
+const methodOverride = require('method-override')
 
 app.set('view engine', 'pug');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(methodOverride('_method'))
 app.use('/items', itemRoutes);
 
 app.get('/', (req, res, next) => {
     return res.redirect('/items');
+});
+
+app.use((err, req, res, next) => {
+    return res.status(404).render('404')
 });
 
 app.listen(PORT, () => {
--- node/03_express_crud_pug/routes/items.js
+++ node/03_express_crud_pug/routes/items.js
@@ -1,20 +1,42 @@
 const express = require('express');
 const router = express.Router();
 
-let items = [];
+let items = {1: {id: 1, name: 'milk', price: '$2.49'},
+            2: {id: 2, name: 'bread', price: '$3.99'}};
+let counter = 1;
 
 router.get('/', (req, res, next) => {
     return res.render('index', {items})
+});
+
+router.post('/', (req, res, next) => {
+    items[counter] = { id: counter, name: req.body.inputItem, price: req.body.inputPrice};
+    counter++;
+    return res.redirect('/items')
 });
 
 router.get('/new', (req, res, next) => {
     return res.render('new');
 });
 
-router.post('/', (req, res, next) => {
-    console.log(req.body)
-    items.push({ item: req.body.inputItem, price: req.body.inputPrice})
-    return res.redirect('/items')
+router.get('/:id', (req, res, next) => {
+    let item = items[req.params.id];
+    return res.render('show', {item});
+});
+
+router.patch('/:id', (req, res, next) => {
+    items[req.params.id] = { id: req.params.id, name: req.body.inputItem, price: req.body.inputPrice};
+    return res.redirect(`/items/${req.params.id}`)
+});
+
+router.get('/:id/edit', (req, res, next) => {
+    let item = items[req.params.id];
+    return res.render('edit', {item});
+});
+
+router.delete('/:id', (req, res, next) => {
+    delete items[req.params.id];
+    return res.redirect('/items');
 });
 
 module.exports = router;
