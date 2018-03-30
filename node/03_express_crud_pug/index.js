3263d248f701d35b6d472c23fa9599b70561d73f
Paula Goyanes <goyanespaula@gmail.com>
2018-03-29 18:42:24 -0700

    complete no styling or error handling

---
+++ node/03_express_crud_pug/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const itemRoutes = require("./routes/items");
+
+app.set("view engine", "pug");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(methodOverride("_method"));
+app.use("/items", itemRoutes);
+
+app.get("/", function(request, response, next) {
+  return response.redirect("/items");
+});
+
+app.use(function(err, request, response, next) {
+  console.log(err);
+});
+
+app.listen(3002, () => console.log("Server starting on 3002"));
