bfcfcf9375d7e01b50cb814397c418d426f3ea7c
Sunny <kanwalbirss@gmail.com>
2018-03-29 22:30:33 -0700

    finished bonus

---
+++ node/03_express_crud_pug/index.js
@@ -0,0 +1,25 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const itemsRoutes = require("./routes/items");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+
+const app = express();
+app.set("view engine", "pug");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(itemsRoutes);
+app.use(morgan("dev"));
+
+app.get("/", function(request, response) {
+  return response.redirect("/items");
+});
+
+app.use(function(request, response, next) {
+  return response.render("404");
+});
+
+app.listen(3000, function() {
+  console.log("Server starting on 3000");
+});
