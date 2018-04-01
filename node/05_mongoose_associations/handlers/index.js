8a57ec75e5bca8babc70efca5b22c9c2cfd2f4f1
maxnawa31 <mnawa31@gmail.com>
2018-03-31 17:13:42 -0700

    adding files

---
+++ node/05_mongoose_associations/handlers/index.js
@@ -0,0 +1,60 @@
+const { Item } = require('../models/index');
+exports.getItems = function(req, res, next) {
+  return Item.find().then(function(items) {
+    return res.render('index', { items });
+  });
+};
+
+exports.getEditForm = function(req, res, next) {
+  console.log(Item.findById(req.params.id));
+  return Item.findById(req.params.id).then(function(foundItem) {
+    return res.render('edit', { foundItem });
+  });
+};
+
+exports.createNewItemForm = function(req, res, next) {
+  return res.render('new');
+};
+
+exports.showIndividualItem = function(req, res, next) {
+  return Item.findById(req.params.id).then(function(foundItem) {
+    return res.render('show', { foundItem });
+  });
+};
+
+exports.postNewItem = function(req, res, next) {
+  let newItem = new Item(req.body);
+  return newItem.save().then(function() {
+    return res.redirect('/items');
+  });
+};
+
+exports.postEditItem = function(req, res, next) {
+  return Item.findByIdAndUpdate(req.params.id, req.body).then(function() {
+    return res.redirect('/items');
+  });
+};
+
+exports.deleteItem = function(req, res, next) {
+  console.log(req.params.id);
+  return Item.findByIdAndRemove(req.params.id).then(function(data) {
+    console.log(data);
+    return res.redirect('/items');
+  });
+};
+
+exports.deleteAllItems = function(req, res, next) {
+  console.log(req.body);
+};
+
+exports.searchForm = function(req, res, next) {
+  console.log(req.params);
+  return res.render('search');
+};
+
+exports.searchResults = function(req, res, next) {
+  let lookupName = req.query.name;
+  return Item.find({ name: lookupName }).then(function(items) {
+    return res.render('searchresults', { items });
+  });
+};
---
+++ node/05_mongoose_associations/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const override = require('method-override');
+const itemsRoutes = require('./routes/index');
+
+app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+app.use(override('_method'));
+app.use('/items', itemsRoutes);
+app.get('/', function(req, res, next) {
+  res.redirect('/items');
+});
+
+app.listen(3000, function() {
+  console.log('Now running on port 3000');
+});
