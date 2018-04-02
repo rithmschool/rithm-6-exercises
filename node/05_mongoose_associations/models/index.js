46086907c65beda6c5864a2daa017019fe18c9a7
Alex Bush <alexabush7@gmail.com>
2018-04-02 15:04:43 -0700

    set up model

--- node/05_mongoose_associations/models/index.js
+++ node/05_mongoose_associations/models/index.js
@@ -1,24 +1,2 @@
-const mongoose = require('mongoose');
-mongoose.set('debug', true);
-mongoose.Promise = global.Promise;
-
-mongoose
-  .connect('mongodb://localhost/animals-app')
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch(err => {
-    console.log(err);
-  });
-
-const animalSchema = new mongoose.Schema(
-  {
-    name: String,
-    cuteness: Number
-  },
-  { timestamps: true }
-);
-
-const Animal = mongoose.model('Animal', animalSchema);
-
-module.exports = Animal;
+exports.Animal = require('./Animal');
+exports.Owner = require('./Owner');
