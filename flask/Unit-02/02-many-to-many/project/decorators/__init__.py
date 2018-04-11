650915a6da03d4653770025bca3ab51c9eb02320
Alex Bush <alexabush7@gmail.com>
2018-04-10 19:57:22 -0700

    finished crud on tags and tag/message association

--- flask/Unit-02/02-many-to-many/project/decorators/__init__.py
+++ flask/Unit-02/02-many-to-many/project/decorators/__init__.py
@@ -1,6 +1,8 @@
 from functools import wraps
 from flask import flash, session, redirect, url_for
-#decorator functions intented to be used across all routes
+
+# decorator functions intended to be used across all routes
+
 
 def ensure_logged_in(fn):
     @wraps(fn)
@@ -9,7 +11,9 @@ def ensure_logged_in(fn):
             flash("Please log in first")
             return redirect(url_for('users.login'))
         return fn(*args, **kwargs)
+
     return wrapper
+
 
 def prevent_login_signup(fn):
     @wraps(fn)
@@ -18,13 +22,18 @@ def prevent_login_signup(fn):
             flash('You are logged in already!')
             return redirect(url_for('users.index'))
         return fn(*args, **kwargs)
+
     return wrapper
+
 
 def ensure_correct_user(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
         if kwargs.get('user_id') != session.get('user_id'):
             flash("Not Authorized")
-            return redirect(url_for('users.index'))
+            return redirect(
+                url_for('users.show', user_id=kwargs.get('user_id')))
+
         return fn(*args, **kwargs)
+
     return wrapper
