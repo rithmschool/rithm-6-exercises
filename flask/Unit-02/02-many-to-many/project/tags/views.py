2804b6cf366e4334225ff7e96ee4f19f9b8b7aa0
Alex Bush <alexabush7@gmail.com>
2018-04-10 20:12:18 -0700

    finished basic crud on tags

--- flask/Unit-02/02-many-to-many/project/tags/views.py
+++ flask/Unit-02/02-many-to-many/project/tags/views.py
@@ -3,31 +3,30 @@ from project.tags.forms import DeleteForm, NewTagForm
 from project.models import Message, User, Tag
 from project import db
 
-tags_blueprint = Blueprint(
-    'tags',
-    __name__,
-    template_folder='templates'
-)
+tags_blueprint = Blueprint('tags', __name__, template_folder='templates')
+
 
 @tags_blueprint.route('/', methods=["POST"])
 def index():
     if request.method == 'POST':
-        form = NewTagForm(request.form)
-        if form.validate():
+        new_tag_form = NewTagForm(request.form)
+        if new_tag_form.validate():
             content = request.form.get('content')
-            new_message = Tag(content)
-            db.session.add(new_message)
+            new_tag = Tag(content)
+            db.session.add(new_tag)
             db.session.commit()
             flash('Tag added')
             return redirect(url_for('users.index'))
         else:
-            return render_template('./tags/new.html', form=form)
+            return render_template('./tags/new.html', form=new_tag_form)
+
 
 @tags_blueprint.route('/new')
 def new():
     new_tag_form = NewTagForm()
-    new_tag_form.set_choices()
+    # new_tag_form.set_choices()
     return render_template('./tags/new.html', form=new_tag_form)
+
 
 @tags_blueprint.route('/tags/<int:tag_id>', methods=["PATCH", "DELETE"])
 def show(tag_id):
@@ -41,7 +40,8 @@ def show(tag_id):
             flash('Tag edited')
             return redirect(url_for('users.index'))
         else:
-            return render_template('/tags/edit.html', tag=target_tag, form=form)
+            return render_template(
+                '/tags/edit.html', tag=target_tag, form=form)
     if request.method == b'DELETE':
         delete_form = DeleteForm(request.form)
         if delete_form.validate():
@@ -50,9 +50,14 @@ def show(tag_id):
             flash('Tag Deleted')
             return redirect(url_for('users.index'))
 
+
 @tags_blueprint.route('/tags/<int:tag_id>/edit')
 def edit(tag_id):
     target_tag = Tag.query.get(tag_id)
     new_tag_form = NewTagForm(obj=target_tag)
-    new_tag_form.set_choices()
-    return render_template('/tags/edit.html', delete_form=DeleteForm(), tag=target_tag, form=new_tag_form)
+    # new_tag_form.set_choices()
+    return render_template(
+        '/tags/edit.html',
+        delete_form=DeleteForm(),
+        tag=target_tag,
+        form=new_tag_form)
