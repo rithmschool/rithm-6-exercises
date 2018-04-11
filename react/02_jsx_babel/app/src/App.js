a32569b6c5b8c4ca940768c3c277e0c8b9eda919
Daniel Szabo <dgszabo@gmail.com>
2018-04-11 16:42:38 -0700

    Part 1 completed

--- react/02_jsx_babel/app/src/App.js
+++ react/02_jsx_babel/app/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FC from './FirstComponent';
+import SC from './SecondComponent';
+import NC from './NamedComponent';
 
 class App extends Component {
   render() {
@@ -13,6 +16,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <FC/>
+        <SC/>
+        <NC name="Jim"/>
       </div>
     );
   }
