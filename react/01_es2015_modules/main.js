09dcf346c2a68ad8596a64802481af11be74ce28
Niki Esfandiari <nesfandiari37@gmail.com>
2018-04-12 10:48:21 -0700

    Both 01, 02 done

---
+++ react/01_es2015_modules/main.js
@@ -0,0 +1,21 @@
+import { choice, remove } from "./arrayHelpers.js";
+import fruits from "./foods.js";
+
+var fruit = choice(fruits);
+console.log(`I'd like one ${fruit}, please`);
+console.log(`Here you go: ${fruit}`);
+console.log(`Delicious! May I have another?`);
+remove(fruits, fruit);
+console.log(`I'm sorry we are all out.  We have ${fruits} fruits remaining`);
+var fruit = choice(fruits);
+console.log(`I'd like one ${fruit}, please`);
+console.log(`Here you go: ${fruit}`);
+console.log(`Delicious! May I have another?`);
+remove(fruits, fruit);
+console.log(`I'm sorry we are all out.  We have ${fruits} fruits remaining`);
+var fruit = choice(fruits);
+console.log(`I'd like one ${fruit}, please`);
+console.log(`Here you go: ${fruit}`);
+console.log(`Delicious! May I have another?`);
+remove(fruits, fruit);
+console.log(`I'm sorry we are all out.  We have ${fruits} fruits remaining`);
