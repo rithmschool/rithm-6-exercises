1661f3cae7344afafeefa2e8445f531a026789b7
Kelson Warner <kelsonpw35@gmail.com>
2018-04-14 14:10:54 -0700

    todo list skeleton added

---
+++ react/08_react_todo_list/todolist/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import TodoList from './TodoList';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Todo List</h1>
+        </header>
+        <TodoList />
+      </div>
+    );
+  }
+}
+
+export default App;
