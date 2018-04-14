f6d64247c2c068b16c94e885eeca5f0de49681c7
Alex Bush <alexabush7@gmail.com>
2018-04-14 10:34:46 -0700

    trying to get clickHandler to update state

--- react/08_react_todo_list/react-todo/src/ToDoList.js
+++ react/08_react_todo_list/react-todo/src/ToDoList.js
@@ -5,22 +5,67 @@ class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentTodos: Array.from({ length: this.props.Todos.length }, () => {
-        return {
-          title: {}
-        };
-      })
+      // currentTodos: Array.from(
+      //   { length: this.props.Todos.length },
+      //   (item, index) => this.props.Todos[index]
+      // )
+
+      currentTodos: Array.from({ length: this.props.Todos.length }).map(
+        (item, index) => {
+          return { ...this.props.Todos[index], isCompleted: 'false' };
+        }
+      )
+
+      // currentTodos: [
+      //   {
+      //     title: 'morning',
+      //     description: 'it is the morning',
+      //     isCompleted: true
+      //   },
+      //   {
+      //     title: 'afternoon',
+      //     description: 'it is the afternoon',
+      //     isCompleted: false
+      //   }
+      // ]
     };
+    this.markAsComplete = this.markAsComplete.bind(this);
+    // this.handleAdd = this.handleAdd.bind(this);
+    // this.handleAdd = this.handleAdd.bind(this);
   }
 
   // addToDo() {}
   // removeToDo() {}
-  markAsComplete() {}
+  markAsComplete(index) {
+    console.log('clicked');
+    //trying to get click event handler to update state
+    console.log(index);
+    this.setState(prevState => {
+      let todosCopy = [...prevState.currentTodos];
+      todosCopy[index].isComplete = !todosCopy[index].isComplete;
+      return { currentTodos: todosCopy };
+    });
+  }
 
   render() {
-    let Todos = this.props.Todos.map(({ title, description }) => {
-      return <Todo title={title} description={description} />;
-    });
+    let Todos = this.state.currentTodos.map(
+      ({ title, description, isCompleted }, i) => {
+        return (
+          <Todo
+            key={i}
+            title={title}
+            description={description}
+            isCompleted={isCompleted}
+            markAsComplete={this.markAsComplete.bind(this, i)}
+            // onClick={() => console.log('murph')}
+            // onClick={this.markAsComplete}
+          />
+        );
+      }
+    );
+    // let Todos = this.props.Todos.map(({ title, description }) => {
+    //   return <Todo title={title} description={description} />;
+    // });
     return <div>{Todos}</div>;
   }
 }
--- react/08_react_todo_list/react-todo/src/Todo.js
+++ react/08_react_todo_list/react-todo/src/Todo.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const Todo = ({ title, description }) => (
+const Todo = ({ title, description, isCompleted, markAsComplete }) => (
   <div>
     <p>Title: {title}</p>
     <p>Description: {description}</p>
+    <p>Is Completed: {isCompleted}</p>
+    <button onClick={markAsComplete}>Completed</button>
+    {/* <button
+      onClick={e => {
+        console.log('arrow function');
+        console.log(e.target);
+        console.log(e.value);
+      }}
+    >
+      Completed
+    </button> */}
   </div>
 );
 
