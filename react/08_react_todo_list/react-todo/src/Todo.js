5a34d1497d3e45d5d071aeb371564b13668a353b
Zoran Savic <zoransavic@me.com>
2018-04-14 15:35:33 -0700

    to do comopleted

--- react/08_react_todo_list/react-todo/src/Todo.js
+++ react/08_react_todo_list/react-todo/src/Todo.js
@@ -2,6 +2,17 @@ import React, { Component } from "react";
 
 export default class Todo extends Component {
   render() {
-    return <p>{this.props.value}</p>;
+    return (
+      <p className={this.props.done ? "completed" : ""}>
+        <strong>{this.props.title}</strong>: {this.props.description} -
+        <span className="pointer" onClick={this.props.remove}>
+          Delete
+        </span>{" "}
+        /&nbsp;
+        <span className="pointer" onClick={this.props.complete}>
+          Done
+        </span>
+      </p>
+    );
   }
 }
--- react/08_react_todo_list/react-todo/src/TodoList.js
+++ react/08_react_todo_list/react-todo/src/TodoList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Todo from "./Todo.js";
+import NewTodoForm from "./NewTodoForm.js";
 
 export default class TodoList extends Component {
   constructor(props) {
@@ -10,26 +11,51 @@ export default class TodoList extends Component {
     this.addTodo = this.addTodo.bind(this);
   }
 
+  removeTodo(id) {
+    console.log(id);
+    const list = this.state.tasks;
+    list.splice(id, 1);
+    this.setState({
+      tasks: list
+    });
+  }
   addTodo(event) {
     this.setState({
-      tasks: this.state.tasks.concat([event.target.task.value])
+      tasks: this.state.tasks.concat({
+        title: [event.target.title.value],
+        description: [event.target.description.value],
+        done: false
+      })
     });
     event.preventDefault();
     event.target.reset();
     return;
   }
+  completeTodo(id) {
+    console.log(id);
+    const list = this.state.tasks;
+    list[id].done = true;
+    this.setState({
+      tasks: list
+    });
+    console.log(id);
+    console.log(this.state.tasks);
+  }
   render() {
     var taskList = this.state.tasks.map((task, idx) => (
-      <Todo value={task} key={idx} />
+      <Todo
+        title={task.title}
+        description={task.description}
+        done={task.done}
+        key={idx}
+        remove={this.removeTodo.bind(this, idx)}
+        complete={this.completeTodo.bind(this, idx)}
+      />
     ));
     return (
       <div>
-        <form onSubmit={this.addTodo}>
-          <label> Todo List </label>
-          <input type="text" name="task" />
-          <br />
-          <input type="submit" value="Submit" class="App-btn" />
-        </form>
+        <NewTodoForm add={this.addTodo.bind(this)} />
+
         <div>{taskList}</div>
       </div>
     );
