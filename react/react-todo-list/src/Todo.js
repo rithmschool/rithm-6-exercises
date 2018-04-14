90cd7d6ddf0e49ef0b1c41da5180ad5da4aaa3fd
Kelson Warner <kelsonpw35@gmail.com>
2018-04-14 12:58:53 -0700

    initial setup with todo and todolist components

--- react/react-todo-list/src/Todo.js
+++ react/react-todo-list/src/Todo.js
@@ -1,1 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Todo extends Component {
+  render() {
+    return (
+      <li>
+        <h2>{this.props.name}</h2>
+        <p>{this.props.description}</p>
+        <p>Completed: {this.props.complete + ''}</p>
+      </li>
+    );
+  }
+}
+
+Todo.propTypes = {
+  name: PropTypes.string,
+  description: PropTypes.string,
+  complete: PropTypes.bool
+};
+
+export default Todo;
--- react/react-todo-list/src/TodoList.js
+++ react/react-todo-list/src/TodoList.js
@@ -1,5 +1,48 @@
 import React, { Component } from 'react';
+import Todo from './Todo.js';
 
-class TodoList extends Component {}
+class TodoList extends Component {
+  render() {
+    let todos = this.props.items.map(item => {
+      return (
+        <Todo
+          key={item.id}
+          name={item.name}
+          description={item.description}
+          complete={item.complete}
+        />
+      );
+    });
+    return (
+      <div>
+        <h1>Todo List</h1>
+        <ul>{todos}</ul>
+      </div>
+    );
+  }
+}
+
+TodoList.defaultProps = {
+  items: [
+    {
+      id: 1,
+      name: 'Laundry',
+      description: 'Complete all of my laundry.',
+      complete: false
+    },
+    {
+      id: 2,
+      name: 'Dishes',
+      description: 'Do all of the dishes in the sink.',
+      complete: true
+    },
+    {
+      id: 3,
+      name: 'Have fun',
+      description: 'Go to movies with a friend!',
+      complete: false
+    }
+  ]
+};
 
 export default TodoList;
