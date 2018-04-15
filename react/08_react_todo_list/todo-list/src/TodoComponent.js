78b50edefae1642142516544740ca5452325fb17
maxnawa31 <mnawa31@gmail.com>
2018-04-15 16:34:42 -0700

    finished todos

--- react/08_react_todo_list/todo-list/src/TodoComponent.js
+++ react/08_react_todo_list/todo-list/src/TodoComponent.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 export default class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.removeTodo = this.removeTodo.bind(this);
+  }
+
+  removeTodo(e) {
+    this.props.remove(e.target);
+  }
   render() {
     return (
       <li>
         {this.props.title} by {this.props.deadline}
+        <button onClick={this.removeTodo}>X</button>
+        <button onClick={this.finsihed}>Todo Finished</button>
       </li>
     );
   }
--- react/08_react_todo_list/todo-list/src/TodoList.js
+++ react/08_react_todo_list/todo-list/src/TodoList.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
 import TodoComponent from './TodoComponent.js';
 import Newtodoform from './newTodoForm.js';
+import './App.css';
+
 export default class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: [{ title: 'Walk the Dog', deadline: '3pm' }]
+      todos: [
+        { title: 'Walk the Dog', deadline: '3pm' },
+        { title: 'Walk the Dog', deadline: '3pm' },
+        { title: 'Walk the Dog', deadline: '3pm' }
+      ]
     };
     this.handleAdd = this.handleAdd.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
   }
   handleAdd(newTodo) {
     this.setState(prevState => ({
       todos: [newTodo, ...prevState.todos]
     }));
   }
+
+  removeTodo(todo) {
+    let index = this.state.todos.indexOf(todo);
+    let array = this.state.todos;
+    array.splice(index, 1);
+    this.setState(prevState => ({
+      todos: array
+    }));
+  }
+
   render() {
     let allTodos = this.state.todos.map((todo, index) => {
       let content = { title: todo.title, deadline: todo.deadline };
       return (
         <TodoComponent
+          remove={this.removeTodo}
+          index={index}
           key={index}
           title={content.title}
           deadline={content.deadline}
@@ -28,7 +47,7 @@ export default class TodoList extends Component {
     return (
       <div>
         <Newtodoform handleAdd={this.handleAdd} />
-        <ul>{allTodos}</ul>
+        <ul className="todoContainer">{allTodos}</ul>
       </div>
     );
   }
