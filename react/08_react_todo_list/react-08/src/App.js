af6ce3ceaadf5e983fc3f29b8caa01b20df829f6
Leo Ng <leogetsthingsdone@gmail.com>
2018-04-16 15:01:50 -0700

    starting to get first components together

---
+++ react/08_react_todo_list/react-08/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TodoList from './components/TodoList';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TodoList />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react/08_react_todo_list/react-08/src/components/TodoList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Todo from './Todo';
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [
+        { id: 0, task: 'eat' },
+        { id: 1, task: 'sleep' },
+        { id: 2, task: 'wake up happy' },
+        { id: 3, task: 'be grateful' },
+        { id: 4, task: 'code' },
+        { id: 5, task: 'kick ass' },
+        { id: 6, task: 'chew bubblegum' }
+      ]
+    };
+  }
+
+  render() {
+    var todos = this.state.todos.map(taco => <li> {taco.task} </li>);
+
+    const someStyle = { float: 'left' };
+
+    // var todos = this.state.todos.map(function(taco, idx) {
+    //   return <li> {taco.task} </li>;
+    // });
+
+    return <div style={someStyle}> {todos} </div>;
+  }
+}
+
+export default TodoList;
