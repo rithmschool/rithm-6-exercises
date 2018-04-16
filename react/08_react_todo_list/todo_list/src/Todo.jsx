99cd5d68f95525327c48bf71b127a01e18ebfb5d
Karl <karlsecco@gmail.com>
2018-04-16 13:54:36 -0700

    adding toggleComplete feature and descriptions

--- react/08_react_todo_list/todo_list/src/Todo.jsx
+++ react/08_react_todo_list/todo_list/src/Todo.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import './Todo.css';
 
 export default class Todo extends Component {
   render() {
     return (
       <li>
-        {this.props.todo}
-        <button onClick={this.props.removeTodo}>Done!</button>
+        <span className={this.props.isComplete ? 'Todo--isComplete' : 'Todo--notComplete'}>
+          {this.props.title}: {this.props.description}
+        </span>{' '}
+        <button onClick={this.props.toggleIsComplete}>Mark Complete</button>{' '}
+        <button onClick={this.props.removeTodo}>Remove!</button>
       </li>
     );
   }
--- react/08_react_todo_list/todo_list/src/TodoList.jsx
+++ react/08_react_todo_list/todo_list/src/TodoList.jsx
@@ -4,18 +4,50 @@ import Todo from './Todo';
 export default class TodoList extends Component {
   constructor(props) {
     super(props);
-    this.state = { todos: ['Eat', 'Sleep', 'Code'] };
+    this.state = {
+      todos: [
+        {
+          title: 'Eat',
+          description: 'Think of all the things that can be vehicles for butter',
+          isComplete: false
+        },
+        {
+          title: 'Sleep',
+          description: 'You need both physical and mental repair, else suffer in performance',
+          isComplete: false
+        },
+        {
+          title: 'Code',
+          description: "Let's remember why we're hear; eyes on the prize",
+          isComplete: false
+        }
+      ]
+    };
+  }
+
+  toggleIsComplete(i) {
+    const { todos } = this.state;
+    todos[i].isComplete = !todos[i].isComplete;
+    this.setState({ todos });
   }
 
   handleRemove(i) {
-    console.log(i);
     const { todos } = this.state;
-    const remainingTodos = todos.slice(0, i).concat(todos.slice(i + 1));
+    const remainingTodos = todos.filter((todo, todoI) => todoI !== i);
     this.setState({ todos: remainingTodos });
   }
 
   render() {
-    let todos = this.state.todos.map((todo, i) => <Todo key={i} todo={todo} removeTodo={this.handleRemove.bind(this, i)} />);
+    let todos = this.state.todos.map((todo, i) => (
+      <Todo
+        key={i}
+        title={todo.title}
+        description={todo.description}
+        isComplete={todo.isComplete}
+        toggleIsComplete={this.toggleIsComplete.bind(this, i)}
+        removeTodo={this.handleRemove.bind(this, i)}
+      />
+    ));
     return <div>{todos}</div>;
   }
 }
