6975f6030c2be73987a41a3965fd70681de58913
RICHARD C BERBERIAN <humblemind@hotmail.com>
2018-04-15 18:42:13 -0700

    added form and complete/delete buttons

--- react/08_react_todo_list/react_todo_list_exercise/src/Todo.jsx
+++ react/08_react_todo_list/react_todo_list_exercise/src/Todo.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import "./Todo.css"
 
 export default class Todo extends Component {
   render() {
     return (
-      <li>
-        {this.props.todoItem}
+      <li className={`todo ${this.props.className}`}>
+        <p>Title: {this.props.title}</p>
+        <p>Description: {this.props.description}</p>
+        <button onClick={this.props.handleComplete}>Mark/Unmark as Complete</button>
+        <button onClick={this.props.handleDelete}>Delete Item</button>
       </li>
     );
   }
 }
 
 Todo.propTypes = {
-  todoItem: PropTypes.string
+  title: PropTypes.string,
+  description: PropTypes.string
 };
--- react/08_react_todo_list/react_todo_list_exercise/src/TodoList.jsx
+++ react/08_react_todo_list/react_todo_list_exercise/src/TodoList.jsx
@@ -1,14 +1,64 @@
 import React, { Component } from "react";
 import Todo from "./Todo";
+import NewTodoForm from "./NewTodoForm";
 
 export default class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          title: 'Buy ice cream',
+          description: 'Grocery Store'
+        },
+        {
+          title: 'Order pizza',
+          description: 'Dinner Plans'
+        },
+        {
+          title: 'Walk 20 miles',
+          description: 'Fitness Routine'
+        }
+      ]
+    };
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd(data) {
+    this.setState(prevState => ({
+      todos: [data, ...prevState.todos]
+    }));
+  }
+
+  handleComplete(idx) {
+    let newState = { ...this.state };
+    if (newState.todos[idx].className === 'complete') {
+      newState.todos[idx].className = '';
+    } else {
+      newState.todos[idx].className = 'complete';
+    }
+    this.setState(newState);
+  }
+
+  handleDelete(idx) {
+    let newState = { ...this.state };
+    newState.todos.splice(idx, 1);
+    this.setState(newState);
+  }
+
   render() {
-    let todoComponents = this.props.todos.map(item => {
-      return <Todo todoItem={item} />
+    let todoComponents = this.state.todos.map((data, idx) => {
+      return <Todo
+        title={data.title}
+        description={data.description}
+        handleComplete={this.handleComplete.bind(this, idx)}
+        handleDelete={this.handleDelete.bind(this, idx)}
+        className={data.className}
+      />
     })
     return (
       <div>
-        <h1>Todo List</h1>
+        <NewTodoForm handleAdd={this.handleAdd} />
         <ol>
           {todoComponents}
         </ol>
@@ -16,7 +66,3 @@ export default class TodoList extends Component {
     );
   }
 }
-
-TodoList.defaultProps = {
-  todos: ['buy ice cream', 'order pizza', 'walk 20 miles']
-};
