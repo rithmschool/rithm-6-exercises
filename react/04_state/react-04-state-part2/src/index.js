6d976651afbb015087af78ea7a28fbe80b9f5660
Paula Goyanes <goyanespaula@gmail.com>
2018-04-15 21:14:06 -0700

    part 2 complete

--- react/04_state/react-04-state-part2/src/index.js
+++ react/04_state/react-04-state-part2/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import CoinContainer from "./CoinContainer";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<CoinContainer />, document.getElementById("root"));
 registerServiceWorker();
