21769d64d4f0414ee241f2907b07f361f25121e1
Alex Bush <alexabush7@gmail.com>
2018-04-17 09:19:07 -0700

    refactoring todo component

--- react/08_react_todo_list/react-todo/src/Todo.js
+++ react/08_react_todo_list/react-todo/src/Todo.js
@@ -13,43 +13,41 @@ class Todo extends Component {
     return {
       title: nextProps.title,
       description: nextProps.description
-    }
+    };
   }
 
   render() {
-    render(
+    const Todo = {
+      //where does this need to happen?
+      this.props.title,
+      this.props.description,
+      this.props.isCompleted,
+      this.props.markAsComplete,
+      this.props.removeToDo,
+      this.props.showEditForm,
+      this.props.editToDo,
+      this.props.isEditSelected
+    };
+    return (
+      <div>
+        <p>Title: {title}</p>
+        <p>Description: {description}</p>
 
-    )
+        <p>Is Completed: {isCompleted ? 'True' : 'False'}</p>
+        <button onClick={markAsComplete}>Toggle Completion Status</button>
+        <button onClick={removeToDo}>Delete</button>
+        <button onClick={showEditForm}>Change</button>
+        {isEditSelected ? (
+          <form onSubmit={editToDo}>
+            <label htmlFor="title">Title: </label>
+            <input name="title" />
+            <label htmlFor="description">Description: </label>
+            <input name="description" />
+            <input type="submit" />
+          </form>
+        ) : null}
+      </div>
+    );
   }
-  //refactor into standard component function
-const Todo = ({
-  title,
-  description,
-  isCompleted,
-  markAsComplete,
-  removeToDo,
-  showEditForm,
-  editToDo,
-  isEditSelected
-}) => (
-  <div>
-    <p>Title: {title}</p>
-    <p>Description: {description}</p>
-
-    <p>Is Completed: {isCompleted ? 'True' : 'False'}</p>
-    <button onClick={markAsComplete}>Toggle Completion Status</button>
-    <button onClick={removeToDo}>Delete</button>
-    <button onClick={showEditForm}>Change</button>
-    {isEditSelected ? (
-      <form onSubmit={editToDo}>
-        <label htmlFor="title">Title: </label>
-        <input name="title" />
-        <label htmlFor="description">Description: </label>
-        <input name="description" />
-        <input type="submit" />
-      </form>
-    ) : null}
-  </div>
-);
 }
 export default Todo;
