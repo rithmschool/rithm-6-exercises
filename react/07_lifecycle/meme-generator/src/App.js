62fda9a6d3ecfafa37aead23bf20671a99ed2a18
Alex Bush <alexabush7@gmail.com>
2018-04-16 19:29:00 -0700

    refactored so that app is at the top of the hierarchy

--- react/07_lifecycle/meme-generator/src/App.js
+++ react/07_lifecycle/meme-generator/src/App.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 import MemeForm from './MemeForm';
 import MemeList from './MemeList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      memes: []
+    };
+    this.addGif = this.addGif.bind(this);
+  }
+
+  async componentDidMount(query) {
+    let data = await axios.get(
+      'https://api.giphy.com/v1/gifs/random?api_key=lB6ofTMau0V63MDSHpV1nVpahYNJkWEd'
+    );
+    let newUrl = data['data']['data']['image_url'];
+    this.setState(prevState => {
+      return { memes: [...prevState.memes, newUrl] };
+    });
+  }
+
+  //add gif
+  async addGif(query) {
+    query = query.split(' ').join('+');
+    let data = await axios.get(
+      'https://api.giphy.com/v1/gifs/search?q=${query}&api_key=lB6ofTMau0V63MDSHpV1nVpahYNJkWEd'
+    );
+  }
+
   render() {
     return (
       <div className="App">
--- react/07_lifecycle/meme-generator/src/MemeList.js
+++ react/07_lifecycle/meme-generator/src/MemeList.js
@@ -6,36 +6,21 @@ class MemeList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      memes: []
+      memeUrls: []
     };
-    this.addGif = this.addGif.bind(this);
   }
 
-  //default gif
-  async componentDidMount(query) {
-    let data = await axios.get(
-      'https://api.giphy.com/v1/gifs/random?api_key=lB6ofTMau0V63MDSHpV1nVpahYNJkWEd'
-    );
-    let newUrl = data['data']['data']['image_url'];
-    console.log('NEW URL STUFF', newUrl);
-    this.setState(prevState => {
-      return { memes: [...prevState.memes, newUrl] };
-    });
-  }
-
-  //add gif
-  async addGif(query) {
-    query = query.split(' ').join('+');
-    let data = await axios.get(
-      'https://api.giphy.com/v1/gifs/search?q=${query}&api_key=lB6ofTMau0V63MDSHpV1nVpahYNJkWEd'
-    );
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return {
+      memeUrls: nextProps.memeUrls
+    };
   }
 
   render() {
-    const memes = this.state.memes.map((url, index) => {
+    const memeUrls = this.state.memeUrls.map((url, index) => {
       return <Meme key={index} src={url} />;
     });
-    return <div>{memes}</div>;
+    return <div>{memeUrls}</div>;
   }
 }
 
