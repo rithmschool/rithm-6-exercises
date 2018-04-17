6365ff6bb14115ce15a58b72ef5c50be44ae3d4a
Alex Bush <alexabush7@gmail.com>
2018-04-17 14:55:59 -0700

    set up basic routes

--- react/10_react_router_vending_machine/vending-machine/src/VendingMachine.js
+++ react/10_react_router_vending_machine/vending-machine/src/VendingMachine.js
@@ -4,7 +4,14 @@ import './App.css';
 
 class VendingMachine extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <Route path="/" exact component={Home} />
+        <Route path="/fish" component={Fish} />
+        <Route path="/squid" component={Squid} />
+        <Route path="/manSquid" component={ManSquid} />
+      </div>
+    );
   }
 }
 
@@ -28,13 +35,25 @@ const Home = () => (
   </div>
 );
 
-const Instructor = props => {
-  const { school, name } = props.match.params;
-  return (
-    <h1>
-      Hi my name is {name} and I am an instructor at {school}!
-    </h1>
-  );
-};
+const Fish = () => (
+  <div>
+    <p>Delicious fish</p>
+    <Link to="/">Take me home</Link>
+  </div>
+);
+
+const Squid = () => (
+  <div>
+    <p>Delicious squid</p>
+    <Link to="/">Take me home</Link>
+  </div>
+);
+
+const ManSquid = () => (
+  <div>
+    <p>Delicious manSquid</p>
+    <Link to="/">Take me home</Link>
+  </div>
+);
 
 export default VendingMachine;
