ec26144b4537bbc43cf94e3b7969417d3e29c6e6
Charmaine Estacio <charmaine.estacio@gmail.com>
2018-04-17 08:41:26 -0700

    can grab url link, still can't render gif on page

--- react/07_lifecycle/giphy-app/src/GiphyList.js
+++ react/07_lifecycle/giphy-app/src/GiphyList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Gif } from './Gif';
 import { NewGiphyForm } from './NewGiphyForm';
+import axios from 'axios';
 
 class GiphyList extends Component {
   constructor(props) {
@@ -16,21 +17,42 @@ class GiphyList extends Component {
     this.handleAdd = this.handleAdd.bind(this);
   }
 
-  handleAdd(newGif) {
+  // handleAdd(newGif) {
+  //   this.setState(prevState => ({
+  //     gifs: [newGif, ...prevState.gifs]
+  //   }));
+  // }
+
+  async handleAdd(query) {
+    query = query.searchTerm.split(' ').join('+');
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=NHtJ3UgX5Gw7MME74ygYPesfA8FFM4qq`
+    );
+    const json = await response.json();
+    let newUrl = json.data[0].url;
+    console.log(newUrl);
     this.setState(prevState => ({
-      gifs: [newGif, ...prevState.gifs]
+      gifs: [newUrl, ...prevState.gifs]
     }));
   }
 
   render() {
-    let allGifs = this.state.gifs.map((gif, i) => (
-      <Gif key={i} src={gif.src} searchTerm={gif.searchTerm} />
-    ));
+    let data = 'Sorry, no gifs yet. Add a gif!';
+    if (this.state.gifs.length > 0) {
+      data = this.state.gifs.map(gif => (
+        <div key={gif.id}>
+          <p>{gif.searchTerm}</p>
+        </div>
+      ));
+    }
+    // let allGifs = this.state.gifs.map((gif, i) => (
+    //   <Gif key={i} src={gif.src} searchTerm={gif.searchTerm} />
+    // ));
     return (
       <div className="GiphyList">
         <h1>Here ARE SOME AWESOME GIFS</h1>
         <NewGiphyForm handleAdd={this.handleAdd} />
-        {allGifs}
+        {data}
       </div>
     );
   }
