49afae57f502b5dc4a81cbd3a85f2a610ee0124d
Daniel Szabo <dgszabo@gmail.com>
2018-04-16 18:15:21 -0700

    Daniel 07 lifecycle - add and remove func added

--- react/07_lifecycle/giphy-app/src/App.jsx
+++ react/07_lifecycle/giphy-app/src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SearchForm from './SearchForm';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
@@ -15,12 +16,18 @@ class App extends Component {
   
   handleSubmit(event) {
     event.preventDefault();
-    console.log('link and button active')
+    let searchTerm = event.target.searchTerm.value.split(' ').join('+');
+    axios
+      .get(`https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=dc6zaTOxFJmzC&limit=1`)
+      .then(res => {
+        let url = res.data.data[0].images.original.url;
+        this.setState(prevState => ({ gifs: [url, ...prevState.gifs ]}))
+      })
   }
 
   removeAll(event) {
       event.preventDefault();
-      console.log('link and button active')
+      this.setState(prevState => ({ gifs: [ ] }))
   }
   
   render() {
@@ -30,19 +37,19 @@ class App extends Component {
           <div className="col page-title">
             <h1 className="m-4">GIPHY PARTY</h1>
           </div>
-          <div className="col">
-            <form className="form-inline">
+          <div className="col text-center">
+            <form className="form-inline d-flex justify-content-center" onSubmit={this.handleSubmit.bind(this)}>
               <div className="form-group mx-sm-3 mb-2">
                 <label htmlFor="searchTerm" className="sr-only">Search term</label>
-                <input type="text" className="form-control" name="serchTerm" id="serchTerm" placeholder="Enter a searct term" />
+                <input type="text" className="form-control" name="searchTerm" id="serchTerm" placeholder="Enter a searct term" />
               </div>
-              <button type="submit" className="btn btn-secondary m-1 mb-2 search-btn" onClick={this.handleSubmit.bind(this)}>Search Giphy</button>
+              <button type="submit" className="btn btn-secondary m-1 mb-2 search-btn">Search Giphy</button>
               <button type="submit" className="btn btn-danger m-1 mb-2 remove-btn" onClick={this.removeAll.bind(this)}>Remove Images</button>
             </form>
           </div>
           
-          <div className="row">
-            {this.state.gifs.map(src => <div className="col"><img src={src} alt="some gif"/></div>)}
+          <div className="row d-flex justify-content-center">
+            {this.state.gifs.map(src => <div className="m-2 img-div"><img className="gif-img" src={src} alt="some gif"/></div>)}
           </div>
         </div>
       </div>
