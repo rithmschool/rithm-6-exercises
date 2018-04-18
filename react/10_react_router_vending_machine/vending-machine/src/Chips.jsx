da0169c8890de5a9faf96e0cd55ca415f01d046d
Daniel Szabo <dgszabo@gmail.com>
2018-04-17 20:46:04 -0700

    Daniel 10 rect router VM - exercise completed

--- react/10_react_router_vending_machine/vending-machine/src/Chips.jsx
+++ react/10_react_router_vending_machine/vending-machine/src/Chips.jsx
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
 import './Chips.css';
+import lays from './lays-flamin-hot.png'
 import { Link } from 'react-router-dom';
 
 class Chips extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            bags: 0
+            bags: [ ]
         }
+        this.addOne = this.addOne.bind(this)
     }
     
+    addOne() {
+        this.setState(prevState => {
+            let top = Math.round(Math.random() * 100);
+            let left = Math.round(Math.random() * 100);
+            let newBags = [ ...prevState.bags, {top, left} ];
+            return {bags: newBags}
+        });
+    }
+
     render() {
         return (
             <div className="chips-comp">
                 <div className="chips-msg">
-                    <h1>BAGS EATEN: {this.state.bags}</h1>
-                    <button>NUM NUM NUM</button>
+                    <h1>BAGS EATEN: {this.state.bags.length}</h1>
+                    <button onClick={this.addOne}><h3>NOM NOM NOM</h3></button>
                     <h1>
                         <Link to="/">Go home!</Link>
                     </h1>
+                </div>
+                <div className="chips-holder">
+                    {this.state.bags.map(el => <img src={lays} className="chips-bag" style={{top: `${el.top}vh`, left: `${el.left}vw`}} />)}
                 </div>
             </div>
         )
