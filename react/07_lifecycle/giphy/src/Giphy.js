177d00ece1158700c172cb44773b982ad2e42c97
Kelson Warner <kelsonpw35@gmail.com>
2018-04-18 16:55:59 -0700

    added giphy

--- react/07_lifecycle/giphy/src/Giphy.js
+++ react/07_lifecycle/giphy/src/Giphy.js
@@ -1,1 +1,7 @@
-            
+import React from 'react';
+
+export default const Giphy = ({ url }) => (
+  <div className="mx-3 my-auto img-div">
+    <img src={url} alt="A lovely giphy" className="m-1 gif-img" />
+  </div>
+);
