d42e28fd9a28a70fca1fd1841be7332a3acfea12
Paula Goyanes <goyanespaula@gmail.com>
2018-04-17 17:09:39 -0700

    complete, no styling or error handling

--- react/11_react_router_calculator/paula-react-11-router-calculator/src/App.js
+++ react/11_react_router_calculator/paula-react-11-router-calculator/src/App.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Result from "./Result";
+import Calculator from "./Calculator";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Route path="/" exact component={Calculator} />
+        <Route path="/:operation/:num1/:num2" component={Result} />
       </div>
     );
   }
---
+++ react/11_react_router_calculator/paula-react-11-router-calculator/src/Calculator.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Calculator extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Calculator</h1>
+        <h3>
+          Use routes to make a calculation like this: /[operation]/[num1]/[num2]
+        </h3>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
