801de858a95b2046ed29b403901a327f759ff98b
Sunny <kanwalbirss@gmail.com>
2018-04-18 23:48:32 -0700

    added calculator component

--- react/11_react_router_calculator/calculator/src/App.js
+++ react/11_react_router_calculator/calculator/src/App.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import Calculator from "./Calculator";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">The Calculator</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Route exact path="/:operation/:num1/:num2" component={Calculator} />
+
+        {/* <Route exact path="/add/:a/:b" component={add} />
+        <Route exact path="/subtract/:a/:b" component={subtract} />
+        <Route exact path="/multiply/:a/:b" component={multiply} />
+        <Route exact path="/divide/:a/:b" component={divide} /> */}
+
+        <p className="App-intro">Lets get started with Calculator.</p>
       </div>
     );
   }
 }
 
 export default App;
+
+// /add/1/2 - should render a component that displays 3.
+// /subtract/3/2 - should render a component that displays 1.
+// /multiply/6/4 - should render a component that displays 24.
+// /divide/20/5 - should render a component that displays 4.
