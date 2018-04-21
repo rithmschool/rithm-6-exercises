b30a067f4febe75953e8e102f6331ec7be803848
Sunny <kanwalbirss@gmail.com>
2018-04-21 16:08:22 -0700

    added components

---
+++ react/10_react_router_vending_machine/vending-machine/src/Components/VendingMachine.js
@@ -0,0 +1,7 @@
+"Hello I am a vending machine. What would you like to eat?";
+
+"Bags Eaten";
+
+"Mangoes: King of Fruit";
+"Never eat potato chips without first opening the packet";
+"That was the best ice-cream soda I ever tasted.";
