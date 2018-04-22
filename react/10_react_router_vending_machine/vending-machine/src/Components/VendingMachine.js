f5512b741180b31b2782596a46335d027ce3a587
Sunny <kanwalbirss@gmail.com>
2018-04-21 20:45:45 -0700

    removed classes

--- react/10_react_router_vending_machine/vending-machine/src/Components/VendingMachine.js
+++ react/10_react_router_vending_machine/vending-machine/src/Components/VendingMachine.js
@@ -2,18 +2,14 @@ import React, { Component } from "react";
 import vending from "./vending.jpg";
 import "./VendingMachine.css";
 
-class VendingMachine extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Hello I am a vending machine. What would you like to eat?</h2>
-        <div
-          className="VendingMachine"
-          style={{ backgroundImage: `url(${vending})` }}
-        />
-      </div>
-    );
-  }
-}
+const VendingMachine = props => (
+  <div>
+    <h2>Hello I am a vending machine. What would you like to eat?</h2>
+    <div
+      className="VendingMachine"
+      style={{ backgroundImage: `url(${vending})` }}
+    />
+  </div>
+);
 
 export default VendingMachine;
