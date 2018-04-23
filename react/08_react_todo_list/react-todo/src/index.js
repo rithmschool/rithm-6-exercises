19481664c975e52f88ab3e2947d7540eaa9ba1d4
Alex Bush <alexabush7@gmail.com>
2018-04-22 19:36:25 -0700

    added react-redux

--- react/08_react_todo_list/react-todo/src/index.js
+++ react/08_react_todo_list/react-todo/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+// import { createStore } from 'redux';
+// import rootReducer from './rootReducer';
+// import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
