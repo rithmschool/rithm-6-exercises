824e5b9c74ec02fc8512a5ca1ee19fc9b84a4b31
Kelson Warner <kelsonpw35@gmail.com>
2018-04-23 15:14:54 -0700

    initial setup, previous app was corrupted, remaking for more experience

---
+++ react/13_react_router_color_factory/color-factory/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Route, Link, Switch, Redirect } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+import NewForm from './NewForm';
+import ColorList from './ColorList';
+import Color from './Color';
+
+class App extends Component {
+  render() {
+    return (
+      s
+    );
+  }
+}
+
+export default App;
