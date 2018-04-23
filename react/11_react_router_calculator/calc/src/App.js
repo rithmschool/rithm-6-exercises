6caca237cf38a91593fcb1478f44a89955c200c3
Kelson Warner <kelsonpw35@gmail.com>
2018-04-22 22:03:04 -0700

    finshed calc app

--- react/11_react_router_calculator/calc/src/App.js
+++ react/11_react_router_calculator/calc/src/App.js
@@ -2,30 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route } from 'react-router-dom';
-
-var result;
-const Calc = props => {
-  console.log(props);
-  console.log(props.location);
-  let { operation, a, b } = props.match.params;
-  if (operation === 'add') return +a + +b;
-  if (operation === 'subtract') return +a - +b;
-  if (operation === 'divide') return +a / +b;
-  if (operation === 'multiply') return +a * +b;
-  return 'Invalid Operation';
-};
+import Calc from './Calc';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        Welcole to the Calculator Application!
+        <br />
+        <br />
+        <br />
+        <br />
         <Route path="/:operation/:a/:b" component={Calc} />
       </div>
     );
