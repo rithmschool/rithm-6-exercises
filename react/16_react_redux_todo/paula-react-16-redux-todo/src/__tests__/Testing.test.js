1bb29d27b00c4908bb267c51073710da2e5505b7
Paula Goyanes <goyanespaula@gmail.com>
2018-04-24 19:24:23 -0700

    starting testing

---
+++ react/16_react_redux_todo/paula-react-16-redux-todo/src/__tests__/Testing.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../App";
+
+// it("renders without crashing", () => {
+//   const div = document.createElement("div");
+//   ReactDOM.render(<App />, div);
+// });
+
+// test("it works", function() {
+//   expect(1).toBe(1);
+// });
+
+// test("arithmetic", function() {
+//   expect(4 + 4).toBeGreaterThan(7);
+//   expect(4 + 4).toBeLessThan(9);
+// });
+
+// test("references", function() {
+//   var arr = [1, 2, 3];
+//   expect(arr).toEqual([1, 2, 3]);
+//   expect(arr).not.toBe([1, 2, 3]); // since === doesn't do deep comparison!
+//   expect(arr).toContain(1);
+// });
